b6dc312c5ca286b15ef4adde7f616add6f41a557
Vanya <vanassssssssss@gmail.com>
2022-06-24 21:49:05 +0300

    fix LoginUser component

--- components/LoginUser/index.tsx
+++ components/LoginUser/index.tsx
@@ -1,17 +1,12 @@
 import { FC, useState } from "react"
-import Link from "next/link"
 import Router from 'next/router'
-import { Avatar, Button, Card, Dropdown, Image, Menu } from "antd"
+import { Avatar, Button, Card, Image } from "antd"
 import { UserOutlined } from "@ant-design/icons"
-import { useAppDispatch, useAppSelector } from "../../store/hooks"
 import { signIn, signOut, useSession } from "next-auth/react"
-import s from './style.module.scss'
-import { log } from "console"
-import { useGetUserByEmailQuery } from "../../api/userApi/user.api"
 import RoleSwither from "../roleSwitcher"
+import s from './style.module.scss'
 
-
-const LoginUser = () => {
+const LoginUser: FC = () => {
 
   const { data: session } = useSession()
   // const { data, error, isLoading } = useGetUserByEmailQuery(`${session?.user?.email}`)
@@ -22,17 +17,23 @@ const LoginUser = () => {
     if (session) {
       return (
         <>
-          <Avatar
-            icon={<UserOutlined />}
-            src={<Image src={session?.user?.image} preview={false} style={{ width: 32 }} alt="UserImg" />}
+          <div
             onClick={() => setMenuActive(prevState => !prevState)}
-          />
+          >
+            <Avatar
+              icon={<UserOutlined />}
+              src={<Image src={session?.user?.image || undefined}
+                preview={false}
+                style={{ width: 32 }}
+                alt="UserImg" />}
+            />
+          </div>
           <div onClick={() => setMenuActive(false)} className={`${s.Info} ${menuActive ? s.active : ''}`}>
             <Card onClick={(e) => e.stopPropagation()} className={s.Card}>
               <Avatar
                 size={100}
                 icon={<UserOutlined />}
-                src={<Image src={session?.user?.image} preview={false} alt="UserImg" />}
+                src={<Image src={session?.user?.image || undefined} preview={false} alt="UserImg" />}
               />
               <h2>{session?.user?.name}</h2>
               <p>{session?.user?.email}</p>
