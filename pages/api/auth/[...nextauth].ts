c76f833eb0c8bdfc0d09d5149aaba3ad0c9a4b3e
Vanya <vanassssssssss@gmail.com>
2022-06-25 03:13:59 +0300

    dev

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,6 +7,38 @@ import jwt from "jsonwebtoken"
 import clientPromise from "../../../lib/mongodb"
 
 export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise,),
+  secret: process.env.NEXTAUTH_SECRET,
+  jwt: {
+    encode: async ({ secret, token }) => {
+      return jwt.sign(token as any, secret);
+    },
+    decode: async ({ secret, token }) => {
+      return jwt.verify(token as string, secret) as any;
+    },
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID || '',
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET || ''
+    })
+    // ...add more providers here
+  ],
+  pages: {
+    signIn: '/auth/sigin',
+    error: '/auth/sigin', ///auth/error Error code passed in query string as ?error=
+    // signOut: '/auth/signout',
+    // verifyRequest: '/auth/verify-request', // (used for check email message)
+    // newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
       const isAllowedToSignIn = true
@@ -25,37 +57,5 @@ export default NextAuth({
     async jwt({ token, user, account, profile, isNewUser }) {
       return token
     }
-  },
-  adapter: MongoDBAdapter(clientPromise,),
-  secret: process.env.NEXTAUTH_SECRET,
-  jwt: {
-    encode: async ({ secret, token }) => {
-      return jwt.sign(token as any, secret);
-    },
-    decode: async ({ secret, token }) => {
-      return jwt.verify(token as string, secret) as any;
-    },
-  },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID||'',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET||''
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID||'',
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET||''
-    })
-    // ...add more providers here
-  ],
-  pages: {
-    signIn: '/auth/sigin',
-    // signOut: '/auth/signout',
-    error: '/auth/sigin', ///auth/error Error code passed in query string as ?error=
-    // verifyRequest: '/auth/verify-request', // (used for check email message)
-    // newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
   }
 })
