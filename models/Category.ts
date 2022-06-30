cb599fac3e8326867b18e6f865f7a467ea1122de
Vanya <vanassssssssss@gmail.com>
2022-06-30 15:55:42 +0300

    add category model

---
+++ models/Category.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, Types, model, ObjectId } from 'mongoose'
+
+export interface ICategory {
+  _id: ObjectId;
+  name: string;
+  desription?: string;
+  taskincategory: [];
+}
+
+const CategoryShema = new Schema<ICategory>({
+  name: { type: String, required: true },
+  taskincategory: [{ type: Schema.Types.ObjectId, ref: 'Task', default: [] }],
+  desription: { type: String, default: 'no description' },
+})
+
+const Category = mongoose.models.Category || mongoose.model("Category", CategoryShema);
+export default Category
