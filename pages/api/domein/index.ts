3d68d57edd51005808fc8e6b3d5aea6af005603b
Vanya <vanassssssssss@gmail.com>
2022-07-05 22:42:06 +0300

    domein api

---
+++ pages/api/domein/index.ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import dbConnect from '../../../utils/dbConnect'
+import Domein from '../../../models/Domein'
+
+
+type Data = {
+  data?: any,
+  success: boolean,
+  error?: any,
+}
+
+async function start() {
+  await dbConnect()
+}
+
+start()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case 'GET':
+      try {
+        const domein = await Domein.find({})
+        return res.status(201).json({ success: true, data: domein })
+      } catch (error) {
+        return res.status(400).json({ success: false, error: error })
+      }
+    case 'POST':
+      try {
+        console.log(req.body.name)
+
+        await Domein.create(req.body)
+          .then((domein) => {
+            return res.status(201).json({ success: true, data: domein })
+          })
+          .catch((error) => {
+            throw error + 'Blya'
+          })
+      } catch (error) {
+        return res.status(400).json({ success: false, error: error })
+      }
+  }
+}
+Ы
