c446246933c171a8d474143fbd10668a6791777f
VakotUA <vakotua.g@gmail.com>
2022-07-06 21:07:59 +0300

    sidebar

--- components/Layouts/MainLayout.tsx
+++ components/Layouts/MainLayout.tsx
@@ -2,6 +2,7 @@ import { Layout } from 'antd'
 import Head from 'next/head'
 import MainFooter from '../Footer'
 import MainHeader from '../Header'
+import Sidebar from '../Sidebar'
 import { SearchBar } from '../SearchBar'
 import s from './MainLayout.style.module.scss'
 
@@ -15,14 +16,19 @@ const MainLayout: React.FC<Props> = ({ children }) => {
       <Head>
         <title>Task-monitoring-app</title>
       </Head>
+
       <Layout className={s.Layout}>
         <MainHeader />
+
         <Layout>
+          <Sidebar />
+
           <Layout.Content className={s.Container}>
             <SearchBar className={s.SearchBar} />
             <div className={s.Background}>{children}</div>
           </Layout.Content>
         </Layout>
+
         <MainFooter />
       </Layout>
     </>
