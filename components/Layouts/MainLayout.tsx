35e9a13c35aa63452a70289fe7fd7fa08cdafde8
VakotUA <vakotua.g@gmail.com>
2022-07-08 20:39:13 +0300

    themes to separate file

--- components/Layouts/MainLayout.tsx
+++ components/Layouts/MainLayout.tsx
@@ -7,30 +7,12 @@ import Sidebar from '../Sidebar'
 import { SearchBar } from '../SearchBar'
 import { useSession } from 'next-auth/react'
 import { useAppSelector } from '../../store/hooks'
+import themes from '../../lib/themes.config'
 
 import styles from './MainLayout.style.module.scss'
 
 interface Props {
   children: React.ReactNode
-}
-
-const themes = {
-  light: {
-    textColor: 'rgba(0, 0, 0, 0.8)',
-    shadowColor: '#00000030',
-    backgroundColor: '#f0f2f5',
-    backgroundColorPrimary: '#fff',
-    primaryColor: '#722ed1',
-    primaryColorHover: '#9254de',
-  },
-  dark: {
-    textColor: 'white',
-    shadowColor: '#95959530',
-    backgroundColor: '#010409',
-    backgroundColorPrimary: '#0d1117',
-    primaryColor: '#238636',
-    primaryColorHover: '#2ea043',
-  },
 }
 
 const MainLayout: React.FC<Props> = ({ children }) => {
---
+++ lib/themes.config.ts
@@ -0,0 +1,20 @@
+const themes = {
+  light: {
+    textColor: 'rgba(0, 0, 0, 0.8)',
+    shadowColor: '#00000030',
+    backgroundColor: '#f0f2f5',
+    backgroundColorPrimary: '#fff',
+    primaryColor: '#722ed1',
+    primaryColorHover: '#9254de',
+  },
+  dark: {
+    textColor: 'white',
+    shadowColor: '#95959530',
+    backgroundColor: '#010409',
+    backgroundColorPrimary: '#0d1117',
+    primaryColor: '#238636',
+    primaryColorHover: '#2ea043',
+  },
+}
+
+export default themes
