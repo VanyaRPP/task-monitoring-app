0bae2ac71a51c7fb9c2e27cbbf11443dc79e523a
GorDi004 <gdiana.ua@gmail.com>
2022-07-11 13:18:22 +0300

    homepage commit

--- components/HomePage/index.tsx
+++ components/HomePage/index.tsx
@@ -1,55 +1,48 @@
 import { Button } from 'antd'
 // import Tool from '../../assets/svg/Tool'
-import s from './style.module.scss'
+import style from './style.module.scss'
 import Image from 'next/image'
 import { calculateProvidedBy } from '@reduxjs/toolkit/dist/query/endpointDefinitions'
 import { useRouter } from 'next/router'
 import { AppRoutes } from '../../utils/constants'
+import cityImg from './../../public/city.png'
 
 const HomePage: React.FC = () => {
   const router = useRouter()
   return (
     <>
-      <div className={s.Block}>
-        <div className={s.Home}>Ваш електронний жек: Комунальник</div>
-        <div className={s.Block2}>
-          <div className={s.HalfBlock}>
-            <Image
-              src="/city.png"
-              alt="City"
-              width={700}
-              height={500}
-              layout="fixed"
-            />
-          </div>
-          <div className={s.HalfBlock}>
-            <div className={s.Block3}>
-              <p className={s.Text}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-                ducimus incidunt modi laborum voluptas asperiores id fuga
-                debitis est vitae!
-              </p>
-            </div>
-            <Button
-              type="primary"
-              className={s.Button}
-              onClick={() => {
-                router.push(AppRoutes.AUTH_SIGNIN)
-              }}
-            >
-              LOGIN
-            </Button>
-            <Button
-              type="primary"
-              className={s.Button}
-              onClick={() => {
-                router.push(AppRoutes.CONTACTS)
-              }}
-            >
-              Contact us
-            </Button>
-          </div>
+      <h1 className={style.Header}>Ваш електронний жек: Комунальник</h1>
+      <div className={style.Container}>
+        <div className={style.HalfBlock}>
+          <Image src={cityImg} alt="City" />
         </div>
+        <div className={style.HalfBlock}>
+          <p className={style.Text}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+            ducimus incidunt modi laborum voluptas asperiores id fuga debitis
+            est vitae!
+          </p>
+        </div>
+      </div>
+      <div className={style.Buttons}>
+        <Button
+          type="primary"
+          className={style.Button}
+          onClick={() => {
+            router.push(AppRoutes.AUTH_SIGNIN)
+          }}
+        >
+          Login
+        </Button>
+        <Button
+          type="primary"
+          className={style.Button}
+          onClick={() => {
+            router.push(AppRoutes.CONTACTS)
+          }}
+        >
+          Contact us
+        </Button>
       </div>
     </>
   )
