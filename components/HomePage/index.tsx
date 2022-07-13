623574d8c8e87ac7a5247e2af8afd4cbab31467b
VakotUA <vakotua.g@gmail.com>
2022-07-13 19:44:19 +0300

    homepage image floating animation

--- components/HomePage/index.tsx
+++ components/HomePage/index.tsx
@@ -2,7 +2,9 @@ import { Button } from 'antd'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { AppRoutes } from '../../utils/constants'
-import cityImg from './../../public/city.png'
+// import cityImg from './../../public/city.png'
+import cityImg1 from '../../assets/images/city.png'
+import cityImg2 from '../../assets/images/city2.png'
 import s from './style.module.scss'
 
 const HomePage: React.FC = () => {
@@ -13,7 +15,8 @@ const HomePage: React.FC = () => {
 
       <div className={s.Container}>
         <div className={s.Image}>
-          <Image src={cityImg} alt="City" />
+          <Image src={cityImg1} alt="City" />
+          <Image src={cityImg2} alt="City" />
         </div>
 
         <div className={s.Text}>
