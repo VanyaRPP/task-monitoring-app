1d7265a51ec946df7979a01480ff0bcf06c1d660
VakotUA <vakotua.g@gmail.com>
2022-07-21 16:56:23 +0300

    add domains form template
    admin can draw polygon on the map
    first 2 poits will be placed by geo-marker

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -4,6 +4,7 @@ import { authOptions } from '../api/auth/[...nextauth]'
 import AdminPageCategories from 'common/components/AdminIU/AdminPageCategorie'
 import AdminPageClients from 'common/components/AdminIU/AdminPageClients'
 import AdminPageTasks from 'common/components/AdminIU/AdminPageTasks'
+import AdminPageDomains from 'common/components/AdminIU/AdminPageDomains'
 import s from './style.module.scss'
 
 const { TabPane } = Tabs
@@ -21,7 +22,8 @@ const AdminPage: React.FC = () => {
         <AdminPageTasks />
       </TabPane>
       <TabPane tab="Domains" key="4">
-        Domains
+        <AdminPageDomains />
+        {/* Domains */}
       </TabPane>
     </Tabs>
   )
