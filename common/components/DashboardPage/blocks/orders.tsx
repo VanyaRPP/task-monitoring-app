1964ca3c887ed9099b27556ccd3b897e6da47aa3
Volodymyr Barybin <vohanchib@gmail.com>
2022-07-26 18:16:37 +0300

    connect tasks to dashboard and write helper func

--- common/components/DashboardPage/blocks/orders.tsx
+++ common/components/DashboardPage/blocks/orders.tsx
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Card, Table, Input } from 'antd'
 import { dateToDefaultFormat } from '../../../assets/features/formatDate'
-import { orders as config } from 'common/lib/dashboard.config'
 import useDebounce from '../../../modules/hooks/useDebounce'
+import { useGetAllTaskQuery } from '../../../api/taskApi/task.api'
+import { firstTextToUpperCase } from '../../../../utils/helpers'
+import { useGetUserByIdQuery } from '../../../api/userApi/user.api'
+import UserLink from '../../UserLink'
+import moment from 'moment'
 
 interface Props {
   style?: string
 }
 
+const Master: React.FC<{ id: string }> = ({ id }) => {
+  const { data } = useGetUserByIdQuery(id)
+  const user = data?.data
+  return <UserLink user={user} />
+}
+
 const Orders: React.FC<Props> = ({ style }) => {
-  const [data, setData] = useState(config)
   const [search, setSearch] = useState({ task: '', master: '' })
   const debounced = useDebounce<{ task: string; master: string }>(search)
-
-  useEffect(() => {
-    setData(
-      config.filter(
-        (item) =>
-          item.task.toLowerCase().includes(debounced.task.toLowerCase()) &&
-          item.master.toLowerCase().includes(debounced.master.toLowerCase())
-      )
-    )
-  }, [debounced])
+  const { data } = useGetAllTaskQuery('')
 
   const searchInput = (order: string) => (
     <Input
@@ -34,46 +34,42 @@ const Orders: React.FC<Props> = ({ style }) => {
   const columns = [
     {
       title: searchInput('Завдання'),
-      dataIndex: 'task',
-      key: 'task',
+      dataIndex: 'name',
+      key: 'name',
       width: '35%',
     },
     {
       title: searchInput('Майстер'),
-      dataIndex: 'master',
-      key: 'master',
+      dataIndex: 'executant',
+      key: 'executant',
       width: '25%',
+      render: (text) => (text ? <Master id={text} /> : 'Не назначено'),
     },
     {
       title: 'Дата',
-      dataIndex: 'date',
-      key: 'date',
+      dataIndex: 'deadline',
+      key: 'deadline',
       width: '20%',
       sorter: (a, b) => Date.parse(a.date) - Date.parse(b.date),
-      render: (text) => dateToDefaultFormat(text),
+      render: (text) => moment(text).format('DD-MM hh:mm'),
     },
     {
       title: 'Статус',
       dataIndex: 'status',
       key: 'status',
       width: '20%',
-      filters: [
-        {
-          text: 'Pending',
-          value: 'Pending',
-        },
-        {
-          text: 'Done',
-          value: 'Done',
-        },
-      ],
-      onFilter: (value: string, record) => record.status.indexOf(value) === 0,
+      render: (text) => firstTextToUpperCase(text),
     },
   ]
 
   return (
     <Card className={style} title="Мої замовлення" style={{ flex: '1.5' }}>
-      <Table dataSource={data} columns={columns} pagination={false} />
+      <Table
+        showHeader={false}
+        dataSource={data?.data}
+        columns={columns}
+        pagination={{ responsive: true, size: 'small', pageSize: 6 }}
+      />
     </Card>
   )
 }
