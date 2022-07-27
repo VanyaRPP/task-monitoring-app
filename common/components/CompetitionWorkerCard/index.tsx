93a120cf0b2e42b9564bf4cda123beb7380240e9
Vanya <vanassssssssss@gmail.com>
2022-07-27 19:57:59 +0300

    Update index.tsx

--- common/components/CompetitionWorkerCard/index.tsx
+++ common/components/CompetitionWorkerCard/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import {
   CheckCircleOutlined,
   CloseCircleOutlined,
@@ -7,15 +7,34 @@ import {
 import { Avatar, Button, Card, Image, Rate } from 'antd'
 import { useGetUserByIdQuery } from '../../api/userApi/user.api'
 import s from './style.module.scss'
+import ModalWindow from '../UI/ModalWindow'
 
 interface Props {
   _id: string | any
 }
 
 const CompetitionWorkerCard: FC<Props> = ({ _id }) => {
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false)
+
   const { data } = useGetUserByIdQuery(_id)
   const user = data?.data
   const desc = ['Жахливо', 'Погано', 'Нормально', 'Добре', 'Прекрасно']
+
+  const onCancelModal = () => {
+    setIsModalVisible(false)
+    // form.resetFields()
+  }
+
+  const onSubmiModal = async () => {
+    // const formData = await form.validateFields()
+    // const data = { ...formData, workerid: userData?.data?._id, taskId: taskId }
+    // setIsFormDisabled(true)
+    // addTaskExecutor(data)
+    // form.resetFields()
+    // setIsModalVisible(false)
+    // setIsFormDisabled(false)
+  }
+
   return (
     <div className={s.container}>
       <Card className={s.Card}>
@@ -39,9 +58,24 @@ const CompetitionWorkerCard: FC<Props> = ({ _id }) => {
         <p>Телефон: {user?.tel}</p>
         <Rate disabled defaultValue={user?.rating} />
         <div className={s.btnGroup}>
-          <Button type="primary">Готово</Button>
+          <Button
+            type="primary"
+            onClick={() => setIsModalVisible(!isModalVisible)}
+          >
+            Готово
+          </Button>
         </div>
       </Card>
+      <ModalWindow
+        title={`Завершити завдання з ${user?.name || user?.email}`}
+        isModalVisible={isModalVisible}
+        onCancel={onCancelModal}
+        onOk={onSubmiModal}
+        okText="Готово"
+        cancelText="Cancel"
+      >
+
+      </ModalWindow>
     </div>
   )
 }
