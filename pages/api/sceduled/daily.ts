79a28774cac7d1b48677a32506cd93cc6c26bba4
Vanya <51020174+VanyaRPP@users.noreply.github.com>
2022-08-04 18:59:12 +0300

    ff

--- pages/api/sceduled/daily.ts
+++ pages/api/sceduled/daily.ts
@@ -14,10 +14,12 @@ export default async function handler(
   try {
     await Task.find({}).then((tasks) => {
       tasks.map(async (task: ITask) => {
-        if (task?.status !== TaskStatuses.IN_WORK
-          && task?.status !== TaskStatuses.COMPLETED
-          && task?.status !== TaskStatuses.EXPIRED
-          && task?.status !== TaskStatuses.ARCHIVED) {
+        if (
+          task?.status !== TaskStatuses.IN_WORK &&
+          task?.status !== TaskStatuses.COMPLETED &&
+          task?.status !== TaskStatuses.EXPIRED &&
+          task?.status !== TaskStatuses.ARCHIVED
+        ) {
           if (moment(task?.deadline).isBefore(Date.now())) {
             await Task.findOneAndUpdate(
               { _id: task._id },
