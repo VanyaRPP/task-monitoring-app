4a173ca740ed1de724a77c9e540cc9b300b3e6dc
Greifelt228 <lavrenchukvitaliy70@gmail.com>
2022-08-15 17:59:27 +0300

    addedProfileDrawer

--- common/components/UserLink/index.tsx
+++ common/components/UserLink/index.tsx
@@ -1,25 +1,97 @@
-import { useSession } from 'next-auth/react'
-import Link from 'next/link'
+import { UserOutlined } from '@ant-design/icons'
+import { AppRoutes } from '@utils/constants'
+import {
+  Avatar,
+  Button,
+  Card,
+  Divider,
+  Drawer,
+  Image,
+  Rate,
+  Collapse,
+  List,
+  Empty,
+} from 'antd'
+import { signOut, useSession } from 'next-auth/react'
+import Router from 'next/router'
+import { useState } from 'react'
 import { IUser } from '../../modules/models/User'
+import Feedback from '../FeedbacksCard/feedback'
+import s from './style.module.scss'
 
 const UserLink: React.FC<{ user: IUser }> = ({ user }) => {
-  const { data: session } = useSession()
+  const [visible, setVisible] = useState(false)
+  // const [menuActive, setMenuActive] = useState(false)
+  const { Panel } = Collapse
+  const showDrawer = () => {
+    setVisible(true)
+  }
+
+  const onClose = () => {
+    setVisible(false)
+  }
 
   if (user) {
     return (
-      <Link
-        href={
-          user?.email === session?.user?.email
-            ? '/profile/'
-            : `/profile/${user?._id}`
-        }
-      >
-        {user?.name ?? user?.email}
-      </Link>
+      <>
+        <a onClick={showDrawer}>{user?.name ?? user?.email}</a>
+        <Drawer
+          title={`Профіль користувача ${user?.name}`}
+          placement="right"
+          onClose={onClose}
+          visible={visible}
+        >
+          <Card onClick={(e) => e.stopPropagation()} className={s.Card}>
+            <Avatar
+              size={150}
+              icon={<UserOutlined />}
+              src={
+                <Image
+                  src={user?.image || undefined}
+                  preview={false}
+                  alt="UserImg"
+                />
+              }
+            />
+            <h2>{user?.name}</h2>
+            <Divider />
+            <p>{user?.email}</p>
+            <Divider />
+            <Rate disabled value={user?.rating} />
+            <Divider />
+            <Collapse ghost accordion className={s.Accordion}>
+              <Panel header="Відгуки" key="1">
+                {user?.feedback?.length ? (
+                  <List
+                    className={s.List}
+                    dataSource={user?.feedback}
+                    renderItem={(item, index) => (
+                      <List.Item key={index} className={s.ListItem}>
+                        <Feedback feedback={item} />
+                      </List.Item>
+                    )}
+                  />
+                ) : (
+                  <Empty className={s.Empty} description="Немає даних" />
+                )}
+              </Panel>
+            </Collapse>
+
+            <Divider />
+            <div className={s.Buttons}>
+              <Button
+                type="link"
+                block
+                onClick={() => Router.push(AppRoutes.PROFILE + '/' + user._id)}
+              >
+                Профіль користувача
+              </Button>
+            </div>
+          </Card>
+        </Drawer>
+      </>
     )
   }
-
-  return null
 }
 
 export default UserLink
