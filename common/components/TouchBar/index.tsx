b75ec3e5478bcb7e21f8eb606b8499534b6b550d
GorDi004 <gdiana.ua@gmail.com>
2022-08-16 12:51:55 +0300

    Smth

--- common/components/TouchBar/index.tsx
+++ common/components/TouchBar/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { Select } from 'antd'
+import { Option } from 'antd/lib/mentions'
+import React, { useEffect, useState } from 'react'
+import ListOneTask from '../ListOneTask'
 import { BackButton, TaskButton } from '../UI/Buttons'
 import s from './style.module.scss'
 
 const TouchBar = () => {
+  // const [select, setSelect] = useState('card')
+  // //
+  // const onChange = async (e : )
   return (
     <div className={s.TouchBar}>
       <BackButton />
+      {/* <Select className={s.Select} onChange={(e) => setSelect(e.target.value)}> */}
+      {/* <Select className={s.Select}>
+        <Option value="list">Список</Option>
+        <Option value="card">Картка</Option>
+      </Select> */}
       <TaskButton />
     </div>
   )
