9b0bc4439400e67be7f53386b77c6ba344c3a7a4
Greifelt228 <lavrenchukvitaliy70@gmail.com>
2022-08-23 13:11:17 +0300

    changedBE

--- common/modules/models/User.ts
+++ common/modules/models/User.ts
@@ -57,6 +57,6 @@ const UserSchema = new mongoose.Schema<IUser>({
 
 // const User = mongoose.models.User || mongoose.model('User', UserSchema)
 const User =
-  (mongoose.models.IUser as mongoose.Model<IUser>) ||
+  (mongoose.models.User as mongoose.Model<IUser>) ||
   mongoose.model('User', UserSchema)
 export default User
