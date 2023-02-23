4a9431b975a9e25da5843df15abca5d7ff88d761
Greifelt228 <lavrenchukvitaliy70@gmail.com>
2023-02-23 23:35:16 +0200

    fixed payment table header

--- common/components/UI/PaymentCardHeader/index.tsx
+++ common/components/UI/PaymentCardHeader/index.tsx
@@ -1,13 +1,13 @@
 import { PlusOutlined, SelectOutlined } from '@ant-design/icons'
 import React, { FC, useState } from 'react'
 import { AppRoutes } from '@utils/constants'
-import { Button, Card } from 'antd'
+import { Button } from 'antd'
 import { useRouter } from 'next/router'
 import AddPaymentModal from '@common/components/AddPaymentModal'
-import s from './style.module.scss'
 import { useSession } from 'next-auth/react'
 import { useGetUserByEmailQuery } from 'common/api/userApi/user.api'
 import { Roles } from '@utils/constants'
+import s from './style.module.scss'
 
 const PaymentCardHeader = () => {
   const Router = useRouter()
@@ -27,14 +27,11 @@ const PaymentCardHeader = () => {
         Проплати
         <SelectOutlined className={s.Icon} />
       </Button>
-
-      <Card onClick={(e) => e.stopPropagation()} className={s.Card}>
-        {user?.role === Roles.ADMIN && (
-          <Button type="link" onClick={() => setIsModalOpen(true)}>
-            <PlusOutlined /> Додати оплату
-          </Button>
-        )}
-      </Card>
+      {user?.role === Roles.ADMIN && (
+        <Button type="link" onClick={() => setIsModalOpen(true)}>
+          <PlusOutlined /> Додати оплату
+        </Button>
+      )}
       <AddPaymentModal isModalOpen={isModalOpen} closeModal={closeModal} />
     </div>
   )
