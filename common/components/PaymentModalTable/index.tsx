ece2b87304564f262e6d984b8fe5460aad4c26fb
Greifelt228 <lavrenchukvitaliy70@gmail.com>
2023-03-07 13:26:46 +0200

    remake of invoice form

--- common/components/PaymentModalTable/index.tsx
+++ common/components/PaymentModalTable/index.tsx
@@ -1,237 +1,145 @@
-import React, { FC, Key, ReactNode, useState } from 'react'
-import { Button, Form, Input, InputNumber, Table } from 'antd'
-import { CheckOutlined, CloseOutlined, EditFilled } from '@ant-design/icons'
+import { Input, Table, Button, Tooltip } from 'antd'
+import { useState, useEffect, FC, ChangeEvent } from 'react'
+import { ColumnProps } from 'antd/lib/table'
 import moment from 'moment'
 import s from './style.module.scss'
 
-interface Item {
-  key: string
-  num: number
-  name: string | ReactNode
-  count: number
+interface ITableData {
+  id: string
+  name: string
+  lastAmount?: number
+  amount: number
   price: number
-  sum: number
+  sum?: number
 }
 
-const originData: Item[] = [
+const dataSource: ITableData[] = [
   {
-    key: '1',
-    num: 1,
-    name: (
-      <span className={s.rowText}>
-        За утримання{' '}
-        <span className={s.month}>({moment().format('MMMM')})</span>
-      </span>
-    ),
-    count: 110,
-    price: 500,
-    get sum() {
-      return this.count * this.price
-    },
+    id: '1',
+    name: 'Утримання',
+    amount: 0,
+    price: 0,
   },
   {
-    key: '2',
-    num: 2,
-    name: (
-      <span className={s.rowText}>
-        Розміщення <span className={s.month}>({moment().format('MMMM')})</span>
-      </span>
-    ),
-    count: 90,
-    price: 400,
-    get sum() {
-      return this.count * this.price
-    },
+    id: '2',
+    name: 'Розміщення',
+    amount: 0,
+    price: 0,
   },
   {
-    key: '3',
-    num: 3,
-    name: (
-      <span className={s.rowText}>
-        Електропостачання{' '}
-        <span className={s.month}>({moment().format('MMMM')})</span>
-      </span>
-    ),
-    count: 190,
-    price: 100,
-    get sum() {
-      return this.count * this.price
-    },
+    id: '3',
+    name: 'Електропостачання',
+    lastAmount: 0,
+    amount: 0,
+    price: 0,
+  },
+  {
+    id: '4',
+    name: 'Водопостачання',
+    lastAmount: 0,
+    amount: 0,
+    price: 0,
   },
 ]
 
-interface EditableCellProps {
-  editing: boolean
-  dataIndex: string
-  title: any
-  inputType: 'number' | 'text'
-  record: Item
-  index: number
-  children: ReactNode
-}
-
-const EditableCell: FC<EditableCellProps> = ({
-  editing,
-  dataIndex,
-  title,
-  inputType,
-  record,
-  index,
-  children,
-  ...restProps
-}) => {
-  const inputNode = inputType === 'number' ? <InputNumber /> : <Input />
-
-  return (
-    <td {...restProps}>
-      {editing ? (
-        <Form.Item
-          name={dataIndex}
-          style={{ margin: 0 }}
-          rules={[
-            {
-              required: true,
-              message: `Please Input ${title}!`,
-            },
-          ]}
-        >
-          {inputNode}
-        </Form.Item>
-      ) : (
-        children
-      )}
-    </td>
-  )
-}
-
 const PaymentModalTable: FC = () => {
-  const [form] = Form.useForm()
-  const [data, setData] = useState(originData)
-  const [editingKey, setEditingKey] = useState('')
+  const [tableData, setTableData] = useState(dataSource)
 
-  const isEditing = (record: Item) => record.key === editingKey
+  // useEffect(() => { TODO: set total after first render
+  //   const newData = [...tableData]
+  //   for (let index = 0; index < tableData.length; index++) {
+  //     setTotal(newData, index)
+  //   }
+  //   setTableData(newData)
+  // }, [])
 
-  const edit = (record: Partial<Item> & { key: Key }) => {
-    form.setFieldsValue({
-      count: '',
-      price: '',
-      ...record,
-    })
-    setEditingKey(record.key)
+  const onInputChange = (key, index) => (e: ChangeEvent<HTMLInputElement>) => {
+    const newData = [...tableData]
+    newData[index][key] = Number(e.target.value)
+    setTotal(newData, index)
+    setTableData(newData)
   }
 
-  const cancel = () => {
-    setEditingKey('')
-  }
-
-  const save = async (key: Key) => {
-    try {
-      const row = (await form.validateFields()) as Item
-
-      const newData = [...data]
-      const index = newData.findIndex((item) => key === item.key)
-      if (index > -1) {
-        const item = newData[index]
-
-        newData.splice(index, 1, {
-          ...item,
-          ...row,
-        })
-        setData(newData)
-        setEditingKey('')
-      } else {
-        newData.push(row)
-        setData(newData)
-        setEditingKey('')
-      }
-    } catch (errInfo) {
-      console.log('Validate Failed:', errInfo)
+  const setTotal = (data, index) => {
+    if (data[index]['lastAmount']) {
+      data[index]['sum'] = Number(
+        (data[index]['lastAmount'] - data[index]['amount']) *
+          data[index]['price']
+      )
+    } else {
+      data[index]['sum'] = Number(data[index]['amount'] * data[index]['price'])
     }
   }
 
-  const columns = [
+  const onConfirm = () => {
+    console.log(tableData)
+  }
+
+  const columns: ColumnProps<ITableData>[] = [
     {
       title: '№',
-      dataIndex: 'num',
+      dataIndex: 'id',
+      width: 50,
     },
     {
       title: 'Назва',
       dataIndex: 'name',
-      width: '25%',
+      render: (name) => (
+        <Tooltip title={`${name} (${moment().format('MMMM')})`}>
+          <span className={s.rowText}>
+            {name} <span className={s.month}>({moment().format('MMMM')})</span>
+          </span>
+        </Tooltip>
+      ),
     },
     {
       title: 'К-сть',
-      dataIndex: 'count',
-      width: 'max-content',
-      editable: true,
+      dataIndex: 'amount',
+      render: (text, record, index) => (
+        <Tooltip title={text}>
+          {record.name === 'Електропостачання' ||
+          record.name === 'Водопостачання' ? (
+            <div className={s.doubleInputs}>
+              {/* value={text} */}
+              <Input onChange={onInputChange('lastAmount', index)} />
+              -
+              <Input onChange={onInputChange('amount', index)} />
+            </div>
+          ) : (
+            <Input onChange={onInputChange('amount', index)} />
+          )}
+        </Tooltip>
+      ),
     },
     {
       title: 'Ціна',
       dataIndex: 'price',
-      width: 'max-content',
-      editable: true,
+      render: (text, record, index) => (
+        <Tooltip title={text}>
+          <Input onChange={onInputChange('price', index)} />
+        </Tooltip>
+      ),
     },
     {
       title: 'Сума',
       dataIndex: 'sum',
-      width: 'max-content',
-    },
-    {
-      title: '',
-      dataIndex: '',
-      render: (_: any, record: Item) => {
-        const editable = isEditing(record)
-        return editable ? (
-          <>
-            <Button
-              type="link"
-              icon={<CheckOutlined />}
-              onClick={() => save(record.key)}
-            />
-            <Button type="link" icon={<CloseOutlined />} onClick={cancel} />
-          </>
-        ) : (
-          <Button
-            type="link"
-            icon={<EditFilled />}
-            onClick={() => edit(record)}
-          />
-        )
-      },
+      render: (text, record, index) => <h4>{text}</h4>,
+      width: 80,
     },
   ]
 
-  const mergedColumns = columns.map((col) => {
-    if (!col.editable) {
-      return col
-    }
-    return {
-      ...col,
-      onCell: (record: Item) => ({
-        record,
-        inputType: 'number',
-        dataIndex: col.dataIndex,
-        title: col.title,
-        editing: isEditing(record),
-      }),
-    }
-  })
-
   return (
-    <Form form={form} component={false}>
+    <>
       <Table
-        components={{
-          body: {
-            cell: EditableCell,
-          },
-        }}
-        bordered
-        dataSource={data}
-        columns={mergedColumns}
-        rowClassName="editable-row"
+        rowKey="id"
+        columns={columns}
+        dataSource={tableData}
         pagination={false}
       />
-    </Form>
+      <Button type="primary" onClick={onConfirm}>
+        Get data
+      </Button>
+    </>
   )
 }
 
