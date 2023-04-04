c1e023798ed723077c6639577496d05bd9d5ce8f
Kostia1390 <71783611+Kostia1390@users.noreply.github.com>
2023-04-04 15:02:55 +0300

    wip
    wip

--- common/components/Forms/AddServiceForm/index.tsx
+++ common/components/Forms/AddServiceForm/index.tsx
@@ -13,9 +13,6 @@ type TabPosition = 'left'
 const onChange1: DatePickerProps['onChange'] = (date, dateString) => {
   console.warn(date, dateString)
 }
-const onChange2 = (key: string) => {
-  console.log(key)
-}
 
 interface Props {
   form: FormInstance<any>
@@ -27,103 +24,81 @@ const AddServiceForm: FC<Props> = ({ form }) => {
   const dayjs = require('dayjs')
   const [tabPosition] = useState<TabPosition>('left')
 
-  const items: TabsProps['items'] = [
-    {
-      key: '1',
-      label: ` Рахунок`,
-      children: (
-        <Form form={form} layout="vertical" className={s.Form}>
-          <Form.Item
-            name="payer"
-            label="Місяць та рік"
-            rules={validateField('data')}
-          >
-            <Space direction="vertical">
-              <DatePicker
-                onChange={onChange1}
-                defaultValue={dayjs}
-                picker="month"
-              />
-            </Space>
-          </Form.Item>
-          <Form.Item
-            name="orenda"
-            label="Утримання приміщень"
-            // rules={validateField('required')}
-          >
-            {' '}
-            <InputNumber
-              type="number"
-              style={{ width: '32%' }}
-              className={s.InputNumber}
-            />
-          </Form.Item>
-          <Form.Item
-            name="electricPrice"
-            label="Електроенергія"
-            // rules={validateField('electricPrice')}
-          >
-            {' '}
-            <InputNumber
-              type="number"
-              style={{ width: '32%' }}
-              className={s.InputNumber}
-            />
-          </Form.Item>
-          <Form.Item
-            name="waterPrice"
-            label="Водопостачання"
-            // rules={validateField('required')}
-          >
-            {' '}
-            <InputNumber
-              type="number"
-              style={{ width: '32%' }}
-              className={s.InputNumber}
-            />
-          </Form.Item>
-          <Form.Item
-            name="inflaPrice"
-            label="Індекс інфляції"
-            // rules={validateField('required')}
-          >
-            {' '}
-            <InputNumber
-              type="number"
-              style={{ width: '32%' }}
-              className={s.InputNumber}
-            />
-          </Form.Item>
-          <Form.Item
-            name="description"
-            label="Опис"
-            // rules={validateField('required')}
-          >
-            <Input.TextArea
-              placeholder="Введіть опис"
-              style={{ width: '90%' }}
-              maxLength={256}
-            />
-          </Form.Item>
-        </Form>
-      ),
-    },
-    {
-      key: '2',
-      label: ` Попередній рахунок`,
-      children: <Image className="object-cover" src={Preliminary_account} />,
-    },
-  ]
-
   return (
-    <>
-      <Tabs
-        tabPosition={tabPosition}
-        defaultActiveKey="1"
-        items={items}
-        onChange={onChange2}
-      />
-    </>
+    <Form form={form} layout="vertical" className={s.Form}>
+      <Form.Item
+        name="payer"
+        label="Місяць та рік"
+        rules={validateField('data')}
+      >
+        <Space direction="vertical">
+          <DatePicker
+            onChange={onChange1}
+            defaultValue={dayjs}
+            picker="month"
+          />
+        </Space>
+      </Form.Item>
+      <Form.Item
+        name="orenda"
+        label="Утримання приміщень"
+        // rules={validateField('required')}
+      >
+        {' '}
+        <InputNumber
+          type="number"
+          style={{ width: '32%' }}
+          className={s.InputNumber}
+        />
+      </Form.Item>
+      <Form.Item
+        name="electricPrice"
+        label="Електроенергія"
+        // rules={validateField('electricPrice')}
+      >
+        {' '}
+        <InputNumber
+          type="number"
+          style={{ width: '32%' }}
+          className={s.InputNumber}
+        />
+      </Form.Item>
+      <Form.Item
+        name="waterPrice"
+        label="Водопостачання"
+        // rules={validateField('required')}
+      >
+        {' '}
+        <InputNumber
+          type="number"
+          style={{ width: '32%' }}
+          className={s.InputNumber}
+        />
+      </Form.Item>
+      <Form.Item
+        name="inflaPrice"
+        label="Індекс інфляції"
+        // rules={validateField('required')}
+      >
+        {' '}
+        <InputNumber
+          type="number"
+          style={{ width: '32%' }}
+          className={s.InputNumber}
+        />
+      </Form.Item>
+      <Form.Item
+        name="description"
+        label="Опис"
+        // rules={validateField('required')}
+      >
+        <Input.TextArea
+          placeholder="Введіть опис"
+          style={{ width: '90%' }}
+          maxLength={256}
+        />
+      </Form.Item>
+    </Form>
   )
 }
 
