6aedc55f497001b2aadd6e9443d06ec903ff0e43
Mykola <my.yershov@gmail.com>
2023-05-07 16:04:35 +0300

    fix

--- pages/real-estate/index.tsx
+++ pages/real-estate/index.tsx
@@ -1,4 +1,29 @@
 import withAuthRedirect from '../../common/components/HOC/withAuthRedirect'
+import { AppRoutes } from '../../utils/constants'
+import { unstable_getServerSession } from 'next-auth'
+import { authOptions } from '../api/auth/[...nextauth]'
+import { GetServerSideProps } from 'next'
 import { RealEstate } from '@common/components/DashboardPage'
 
 export default withAuthRedirect(RealEstate)
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  )
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: AppRoutes.AUTH_SIGN_IN,
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {},
+  }
+}
