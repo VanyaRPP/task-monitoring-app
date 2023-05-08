9588c275d4bfb35d09660ea38927d3c8027e90b8
Rukosmotrov <drukosmotrov@gmail.com>
2023-05-08 12:38:11 +0200

    Changed logo of the system

--- common/components/Logo/index.tsx
+++ common/components/Logo/index.tsx
@@ -3,14 +3,16 @@ import { AppRoutes } from '../../../utils/constants'
 import LogoCircle from '../../assets/svg/logo_circle'
 import NewLogo from '../../assets/svg/newLogo'
 import s from './style.module.scss'
+import SpaceHubLogo from '../../assets/svg/spaceHubLogo'
 
 const Logo: React.FC = () => {
   return (
     <Link href={AppRoutes.INDEX}>
       <div className={s.All}>
         {/* <LogoCircle className={s.Logo} /> */}
-        <NewLogo className={s.Logo} />
-        <h1 className={s.Title}>КОМУНАЛЬНИК</h1>
+        {/*<NewLogo className={s.Logo} />*/}
+        {/*<h1 className={s.Title}>КОМУНАЛЬНИК</h1>*/}
+          <SpaceHubLogo className={s.Logo}/>
       </div>
     </Link>
   )
