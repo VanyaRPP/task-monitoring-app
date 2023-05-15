890718403196e887ea5f74d67d1598f7bc7f67f7
YuliaMikha <juliamykhailova2507@gmail.com>
2023-05-15 15:37:05 +0300

    add buttonAdd

--- common/components/UI/DomainsComponents/DomainStreetsComponent/index.tsx
+++ common/components/UI/DomainsComponents/DomainStreetsComponent/index.tsx
@@ -1,34 +1,50 @@
-import React from 'react'
-import OrganistaionsComponents from '../../OrganistaionsComponents'
+import React, { useState } from 'react'
 import s from './style.module.scss'
 import { Table } from 'antd'
+import { Button } from 'antd'
+import { PlusOutlined, SelectOutlined } from '@ant-design/icons'
+import { useRouter } from 'next/router'
 
 const DomainStreetsComponent = ({ data }) => {
+  const Router = useRouter()
+  const [isModalDomainOpen, setIsModalDomainOpen] = useState(false)
+
+  const closeModal = () => {
+    setIsModalDomainOpen(false)
+  }
+
   return (
     <div className={s.tableHeader}>
-      <Table
+      <Button type="link">
+        Домени
+        <SelectOutlined className={s.Icon} />
+      </Button>
+      <Button type="link" onClick={() => setIsModalDomainOpen(true)}>
+        <PlusOutlined /> Додати
+      </Button>
+      {/* <Table
         expandable={{
           expandedRowRender: (data) => <OrganistaionsComponents />,
         }}
         dataSource={testData}
         columns={columns}
         pagination={false}
-      />
+      /> */}
     </div>
   )
 }
 
-const columns = [
-  {
-    title: 'Вулиця',
-    dataIndex: 'street',
-  },
-]
+// const columns = [
+//   {
+//     title: 'Вулиця',
+//     dataIndex: 'street',
+//   },
+// ]
 
-const testData = [
-  {
-    street: '12 Короленка 12',
-  },
-]
+// const testData = [
+//   {
+//     street: '12 Короленка 12',
+//   },
+// ]
 
 export default DomainStreetsComponent
