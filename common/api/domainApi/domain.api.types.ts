d4d154a52c512f62e55abc5ffaaf413c58773c37
YuliaMikha <juliamykhailova2507@gmail.com>
2023-05-21 22:58:59 +0300

    fix streets-string

--- common/api/domainApi/domain.api.types.ts
+++ common/api/domainApi/domain.api.types.ts
@@ -1,19 +1,19 @@
-import { ObjectId } from 'mongoose'
-
 export interface IDomainModel {
   name: string
   address: string
   adminEmails: [string]
-  streets: [ObjectId]
+  streets: string[]
   description: string
   bankInformation: string
   phone: string
   email: string
 }
+
 export interface IExtendedDomain extends IDomainModel {
   _id: string
   _v: number
 }
+
 export interface IAddDomainResponse {
   success: boolean
   data: IExtendedDomain
