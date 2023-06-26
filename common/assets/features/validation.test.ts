7899e671e5205536c8b5e1d32cdcab6f48c0c978
Fedir Melnyk <pluserwork24@gmail.com>
2023-06-26 14:27:00 +0300

    test fixed

--- common/assets/features/validation.test.ts
+++ common/assets/features/validation.test.ts
@@ -1,22 +1,16 @@
 // run in cmd with "yarn test filename | npm test filename"
-const emailRegex =  /^([a-zA-Z0-9_]{6,30})@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-// const emailRegex = /^[a-zA-Z0-9_.]{6,30}@(?!.*\d)[a-zA-Z][a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i;
+import { emailRegex } from "./validators";
 const { expect, test, toBeTruthy, toBeFalsy } = require('@jest/globals');
+// const emailRegex = /^[a-zA-Z0-9_.]{6,30}@(?!.*\d)[a-zA-Z][a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i;
 
 describe('isValidValidation', () => {
     test('email is valid', () => {
         expect(emailRegex.test("pluserwork24@gmail.com")).toBeTruthy();
-    });
-    test("email is valid",()=>{
-        expect(emailRegex.test("my.yershov@gmail.com")).toBeTruthy();
-    });
-    test("email is valid",()=>{
+        expect(emailRegex.test("exampletest@mail.ua")).toBeTruthy();
         expect(emailRegex.test("my.yershov@gmail.com")).toBeTruthy();
     });
     test('email is invalid', () => {
         expect(emailRegex.test("example@2mail.con")).toBeFalsy();
-    });
-    test('email is invalid', () => {
         expect(emailRegex.test("example2!2@<mail.con")).toBeFalsy();
     });
 });
