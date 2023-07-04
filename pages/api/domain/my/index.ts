f479a63faab2090a94e549de2a002056c09e92af
Mykola <my.yershov@gmail.com>
2023-07-04 19:51:56 +0300

    added domains displaying for User

--- pages/api/domain/my/index.ts
+++ pages/api/domain/my/index.ts
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 import type { NextApiRequest, NextApiResponse } from 'next'
-import start, { Data } from 'pages/api/api.config'
+import RealEstate from '@common/modules/models/RealEstate'
 import { getCurrentUser } from '@utils/getCurrentUser'
+import start, { Data } from 'pages/api/api.config'
 import Domain from '@common/modules/models/Domain'
 import { Roles } from '@utils/constants'
 
@@ -21,11 +22,22 @@ export default async function handler(
           const domains = await Domain.find({
             adminEmails: { $in: [user.email] },
           })
+          // TODO: return more fields, add typisation
           // TODO: IDomain
           const data = domains.map((i) => ({ name: i.name }))
           return res.status(200).json({ success: true, data })
         }
-        // TODO: user
+
+        if (user.roles?.includes(Roles.USER)) {
+          const realEstates = await RealEstate.find({
+            adminEmails: { $in: [user.email] },
+          }).populate({ path: 'domain', select: 'name' })
+
+          // TODO: return more fields, add typisation
+          // TODO: IDomain
+          const data = realEstates.map((i) => ({ name: i.domain.name }))
+          return res.status(200).json({ success: true, data })
+        }
         // TODO: global admin (???)
         return res.status(200).json({ success: true, data: [] })
       } catch (error) {
