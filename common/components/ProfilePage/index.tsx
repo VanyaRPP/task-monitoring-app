39743f52ff75dd816272c4a0f05f8b714d177ebe
YuliaMikha <juliamykhailova2507@gmail.com>
2023-07-11 11:06:43 +0300

    DomainCard-for DomainAdmin

--- common/components/ProfilePage/index.tsx
+++ common/components/ProfilePage/index.tsx
@@ -1,39 +1,22 @@
-import React from 'react'
-import { UserOutlined } from '@ant-design/icons'
-import { Avatar, Card, Image, Input } from 'antd'
-import RoleSwitcher from 'common/components/UI/roleSwitcher'
-import s from './style.module.scss'
-import { useGetCurrentUserQuery } from '../../api/userApi/user.api'
-import MyDomain from './my-domain'
+import React from 'react';
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar, Card, Image, Input } from 'antd';
+import RoleSwitcher from 'common/components/UI/roleSwitcher';
+import s from './style.module.scss';
+import { useGetCurrentUserQuery } from '../../api/userApi/user.api';
+import MyDomain from './my-domain';
+import MyDomainsCard from '@components/ProfilePage/my-domainCard';
 
 const ProfilePage: React.FC = () => {
-  const { data: user, isLoading } = useGetCurrentUserQuery()
+  const { data: user, isLoading: userLoading } = useGetCurrentUserQuery();
 
   return (
     <>
       <h2 className={s.Header}>Мій Профіль</h2>
       <div className={s.Container}>
-        <Card
-          loading={isLoading}
-          title={user?.name}
-          className={s.Profile}
-          // extra={
-          //   user?.roles?.includes(Roles.GLOBAL_ADMIN) && (
-          //     <Button
-          //       type="link"
-          //       onClick={() => Router.push(AppRoutes.ADMIN)}
-          //       className={s.Admin}
-          //     >
-          //       Панель Адміністратора
-          //     </Button>
-          //   )
-          // }
-        >
+        <Card loading={userLoading} title={user?.name} className={s.Profile}>
           <div className={s.Avatar}>
-            <Avatar
-              icon={<UserOutlined />}
-              src={<Image src={user?.image || undefined} alt="User" />}
-            />
+            <Avatar icon={<UserOutlined />} src={<Image src={user?.image || undefined} alt="User" />} />
           </div>
           <div className={s.Info}>
             <RoleSwitcher />
@@ -50,10 +33,15 @@ const ProfilePage: React.FC = () => {
             </Card> */}
           </div>
         </Card>
-        {/* <FeedbacksCard user={user} loading={isLoading} userRate={userRate} /> */}
+        
+         <div className={s.DomainMyCard}>
+          <Card title=" " loading={userLoading}>
+          <MyDomainsCard />
+          </Card>
+          </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
---
+++ pages/api/domain/my/index.ts
@@ -0,0 +1,60 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+import type { NextApiRequest, NextApiResponse } from 'next'
+import RealEstate from '@common/modules/models/RealEstate'
+import { getCurrentUser } from '@utils/getCurrentUser'
+import start, { Data } from 'pages/api/api.config'
+import Domain from '@common/modules/models/Domain'
+
+start()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { isDomainAdmin, isUser, user } = await getCurrentUser(req, res)
+
+  switch (req.method) {
+    case 'GET':
+      try {
+        if (isDomainAdmin) {
+          const domains = await Domain.find({
+            adminEmails: { $in: [user.email] },
+          })
+          // TODO: return more fields, add typisation
+          // TODO: IDomain
+            const data = domains.map((i) => ({
+                name: i.name,
+                address: i.address,
+                adminEmails: i.adminEmails,
+                description: i.description,
+                bankInformation: i.bankInformation,
+                phone: i.phone,
+                email: i.email,
+
+            }))
+          return res.status(200).json({ success: true, data })
+        }
+
+        if (isUser) {
+          const realEstates = await RealEstate.find({
+            adminEmails: { $in: [user.email] },
+          }).populate({ path: 'domain', select: 'name' })
+
+          // TODO: return more fields, add typisation
+          // TODO: IDomain
+            const data = realEstates.map((i) => ({ name: i.domain.name,
+            }))
+          return res.status(200).json({ success: true, data })
+        }
+        // TODO: global admin (???)
+        return res.status(200).json({ success: true, data: [] })
+      } catch (error) {
+        return res.status(400).json({ success: false })
+      }
+    default:
+      return res
+        .status(405)
+        .json({ success: false, message: 'Method Not Allowed' })
+  }
+}
