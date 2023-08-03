efcbed646bbe0a65cc5a7e54d028aa6d7fbbcbd9
Mykola <my.yershov@gmail.com>
2023-08-03 15:52:31 +0300

    updated user card

--- common/components/ProfilePage/index.tsx
+++ common/components/ProfilePage/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { UserOutlined } from '@ant-design/icons'
-import { Avatar, Card, Image, Input } from 'antd'
+import { Avatar, Card, Image } from 'antd'
 import RoleSwitcher from 'common/components/UI/roleSwitcher'
 import s from './style.module.scss'
 import { useGetCurrentUserQuery } from '../../api/userApi/user.api'
@@ -8,9 +8,9 @@ import MyDomain from './my-domain'
 import MyCompany from './my-company'
 import MyDomainsCard from './my-domainCard'
 import { Roles } from '@utils/constants'
-        
+
 const ProfilePage: React.FC = () => {
-  const { data: user, isLoading: userLoading } = useGetCurrentUserQuery();
+  const { data: user, isLoading: userLoading } = useGetCurrentUserQuery()
 
   return (
     <>
@@ -18,33 +18,33 @@ const ProfilePage: React.FC = () => {
       <div className={s.Container}>
         <Card loading={userLoading} title={user?.name} className={s.Profile}>
           <div className={s.Avatar}>
-            <Avatar icon={<UserOutlined />} src={<Image src={user?.image || undefined} alt="User" />} />
+            <Avatar
+              icon={<UserOutlined />}
+              src={<Image src={user?.image || undefined} alt="User" />}
+            />
           </div>
           <div className={s.Info}>
             <RoleSwitcher />
-            {user?.roles.includes(Roles.GLOBAL_ADMIN) ? null: <MyDomain />}
-            <MyCompany user={user}/>
+            {!user?.roles.includes(Roles.GLOBAL_ADMIN) && (
+              <>
+                <MyDomain />
+                <MyCompany />
+              </>
+            )}
             <Card size="small" title="Електронна пошта">
               {user?.email}
             </Card>
-            {/* <Card size="small" title="Номер телефону">
-              <Input
-                name="tel"
-                value={user?.tel}
-                placeholder="Введіть номер телефону"
-              />
-            </Card> */}
           </div>
         </Card>
-        
-         <div className={s.DomainMyCard}>
+
+        <div className={s.DomainMyCard}>
           <Card title=" " loading={userLoading}>
-          <MyDomainsCard />
+            <MyDomainsCard />
           </Card>
-          </div>
+        </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default ProfilePage;
+export default ProfilePage
