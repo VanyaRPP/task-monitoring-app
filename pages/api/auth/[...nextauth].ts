891613e0b804ea5ad55a56525d2055422a041223
hacer7 <74371518+hacer7@users.noreply.github.com>
2023-08-04 14:37:02 +0300

    secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -34,7 +34,7 @@ function text({ url, host }) {
 
 export const authOptions: NextAuthOptions = {
   adapter: MongoDBAdapter(clientPromise),
-  secret: process.env.NEXT_PUBLIC_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: 'jwt',
   },
