eff406bde7079bd8ba0b443d81e67fafe5614a87
hacer7 <kolya7mischenchuk7@gmail.com>
2023-08-10 16:28:52 +0300

    console logs and errors added

--- pages/api/real-estate/index.ts
+++ pages/api/real-estate/index.ts
@@ -48,7 +48,8 @@ export default async function handler(
 
         return res.status(200).json({ success: true, data: realEstates })
       } catch (error) {
-        return res.status(400).json({ success: false, message: error })
+        console.log('aws req error', error)
+        return res.status(400).json({ success: false, message: error, error })
       }
 
     case 'POST':
--- pages/api/service/index.ts
+++ pages/api/service/index.ts
@@ -79,7 +79,8 @@ export default async function handler(
           data: services,
         })
       } catch (error) {
-        return res.status(400).json({ success: false, message: error })
+        console.log('aws req error', error)
+        return res.status(400).json({ success: false, message: error, error })
       }
 
     case 'POST':
