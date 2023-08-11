08d9d4970b8e54bb9927f5f2fd2c83d5b309b011
hacer7 <kolya7mischenchuk7@gmail.com>
2023-08-11 14:38:01 +0300

    Update index.tsx

--- pages/real-estate/index.tsx
+++ pages/real-estate/index.tsx
@@ -1,6 +1,6 @@
 import withAuthRedirect from '../../common/components/HOC/withAuthRedirect'
 import { AppRoutes } from '../../utils/constants'
-import { unstable_getServerSession } from 'next-auth'
+import { getServerSession } from 'next-auth'
 import { authOptions } from '../api/auth/[...nextauth]'
 import { GetServerSideProps } from 'next'
 import RealEstateBlock from '@common/components/DashboardPage/blocks/realEstates'
@@ -8,7 +8,7 @@ import RealEstateBlock from '@common/components/DashboardPage/blocks/realEstates
 export default withAuthRedirect(RealEstateBlock)
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await unstable_getServerSession(
+  const session = await getServerSession(
     context.req,
     context.res,
     authOptions
