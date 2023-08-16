3325b6bbfa025810a1cc752f637888029b91ecac
Mykola <my.yershov@gmail.com>
2023-08-16 17:11:43 +0300

    upd

--- common/components/Forms/AddPaymentForm/InvoiceNumber.tsx
+++ common/components/Forms/AddPaymentForm/InvoiceNumber.tsx
@@ -13,7 +13,6 @@ export default function InvoiceNumber({ form, edit }) {
 
   return (
     <Form.Item
-      initialValue={invoiceNumber}
       name="invoiceNumber"
       label="№ інвойса"
     >
