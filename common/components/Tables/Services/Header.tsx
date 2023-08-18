57293e8db1f280870355e5bec9fa9e323f7aa198
VakotUA <90602514+VakotUA@users.noreply.github.com>
2023-08-19 00:17:26 +0300

    services table update

---
+++ common/components/Tables/Services/Header.tsx
@@ -0,0 +1,45 @@
+import { PlusOutlined, SelectOutlined } from '@ant-design/icons'
+import { Button } from 'antd'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+
+import { useGetCurrentUserQuery } from '@common/api/userApi/user.api'
+import AddServiceModal from '@common/components/AddServiceModal'
+import { AppRoutes } from '@utils/constants'
+import { isAdminCheck } from '@utils/helpers'
+
+export interface Props {
+  showAddButton?: boolean
+}
+
+const ServicesHeader: React.FC<Props> = ({ showAddButton = false }) => {
+  const router = useRouter()
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const { data: user } = useGetCurrentUserQuery()
+
+  const openModal = () => setIsModalOpen(true)
+  const closeModal = () => setIsModalOpen(false)
+
+  if (router.query.email)
+    return <span>Оплата від користувача {router.query.email}</span>
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <Button type="link" onClick={() => router.push(AppRoutes.SERVICE)}>
+        Послуги <SelectOutlined />
+      </Button>
+
+      {showAddButton && isAdminCheck(user?.roles) && (
+        <>
+          <Button type="link" onClick={openModal}>
+            <PlusOutlined /> Додати
+          </Button>
+          <AddServiceModal isModalOpen={isModalOpen} closeModal={closeModal} />
+        </>
+      )}
+    </div>
+  )
+}
+
+export default ServicesHeader
