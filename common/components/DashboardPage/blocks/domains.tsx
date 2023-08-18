31cbe675beac01ef23b74e5cc1fe3a188dad4443
VakotUA <90602514+VakotUA@users.noreply.github.com>
2023-08-18 22:39:26 +0300

    domains table update

--- common/components/DashboardPage/blocks/domains.tsx
+++ common/components/DashboardPage/blocks/domains.tsx
@@ -1,96 +1,11 @@
-import React from 'react'
-import cn from 'classnames'
-import { useRouter } from 'next/router'
-import { AppRoutes } from '@utils/constants'
-import s from './style.module.scss'
+import DomainsHeader from '@common/components/Tables/Domains/Header'
+import DomainsTable from '@common/components/Tables/Domains/Table'
 import TableCard from '@common/components/UI/TableCard'
-import {
-  useDeleteDomainMutation,
-  useGetDomainsQuery,
-} from '@common/api/domainApi/domain.api'
-import DomainStreetsComponent from '@common/components/UI/DomainsComponents/DomainStreetsComponent'
-import { Popconfirm, Table, message, Tooltip } from 'antd'
-import { IExtendedDomain } from '@common/api/domainApi/domain.api.types'
-import { DeleteOutlined, QuestionCircleOutlined } from '@ant-design/icons'
-import StreetsBlock from './streets'
 
-const DomainsBlock = ({}) => {
-  const { data: domains, isLoading } = useGetDomainsQuery({})
-  const router = useRouter()
-
-  const [deleteDomain, { isLoading: deleteLoading }] = useDeleteDomainMutation()
-
-  const handleDelete = async (id: string) => {
-    const response = await deleteDomain(id)
-    if ('data' in response) {
-      message.success('Видалено!')
-    } else {
-      message.error('Помилка при видаленні')
-    }
-  }
+const DomainsBlock: React.FC = () => {
   return (
-    <TableCard
-      title={<DomainStreetsComponent data={domains} />}
-      className={cn({ [s.noScroll]: router.pathname === AppRoutes.DOMAIN })}
-    >
-      <Table
-        expandable={{
-          expandedRowRender: (data) => (
-            <div className={cn(s.StreetsBlockContainer)}>
-              <StreetsBlock
-                domainId={data._id}
-                showAddButton={false}
-                height={500}
-              />
-            </div>
-          ),
-        }}
-        columns={[
-          {
-            title: 'Назва',
-            dataIndex: 'name',
-          },
-          {
-            title: 'Адміністратори',
-            dataIndex: 'adminEmails',
-            render: (adminEmails) => <span>{adminEmails.join(', ')}</span>,
-          },
-          {
-            title: 'Опис',
-            dataIndex: 'description',
-            render: (text) => (
-              <Tooltip title={text}>
-                <QuestionCircleOutlined />
-              </Tooltip>
-            ),
-          },
-          {
-            title: '',
-            dataIndex: '',
-            width: '9%',
-            render: (_, domain: IExtendedDomain) => (
-              <div className={s.popconfirm}>
-                <Popconfirm
-                  title={`Ви впевнені що хочете видалити ${
-                    domain.name ?? 'цей домен'
-                  }?`}
-                  onConfirm={() => handleDelete(domain?._id)}
-                  cancelText="Відміна"
-                  disabled={deleteLoading}
-                >
-                  <DeleteOutlined className={s.icon} />
-                </Popconfirm>
-              </div>
-            ),
-          },
-        ]}
-        loading={isLoading}
-        dataSource={domains}
-        pagination={false}
-        bordered
-        size="small"
-        rowKey="_id"
-      />
+    <TableCard title={<DomainsHeader showAddButton />}>
+      <DomainsTable />
     </TableCard>
   )
 }
---
+++ common/components/Tables/Domains/Table.tsx
@@ -0,0 +1,109 @@
+import { DeleteOutlined, QuestionCircleOutlined } from '@ant-design/icons'
+import { Popconfirm, Table, Tooltip, message } from 'antd'
+import { useRouter } from 'next/router'
+import { ColumnType } from 'antd/lib/table'
+
+import {
+  useDeleteDomainMutation,
+  useGetDomainsQuery,
+} from '@common/api/domainApi/domain.api'
+import { IExtendedDomain } from '@common/api/domainApi/domain.api.types'
+import StreetsBlock from '@common/components/DashboardPage/blocks/streets'
+import { AppRoutes } from '@utils/constants'
+
+const DomainsTable: React.FC = () => {
+  const router = useRouter()
+  const isOnPage = router.pathname === AppRoutes.DOMAIN
+
+  const { data, isLoading } = useGetDomainsQuery({})
+
+  const [deleteDomain, { isLoading: deleteLoading }] = useDeleteDomainMutation()
+
+  const handleDelete = async (id: string) => {
+    const response = await deleteDomain(id)
+    if ('data' in response) {
+      message.success('Видалено!')
+    } else {
+      message.error('Помилка при видаленні')
+    }
+  }
+
+  return (
+    <Table
+      rowKey="_id"
+      size="small"
+      pagination={
+        !isOnPage && {
+          responsive: false,
+          size: 'small',
+          pageSize: 8,
+          position: ['bottomCenter'],
+          hideOnSinglePage: true,
+        }
+      }
+      loading={isLoading}
+      columns={getDefaultColumns(handleDelete, deleteLoading)}
+      expandable={{
+        expandedRowRender: ({ _id: domainId }) => (
+          <StreetsBlock domainId={domainId} />
+        ),
+      }}
+      dataSource={data}
+      scroll={{ x: 600 }}
+    />
+  )
+}
+
+const getDefaultColumns = (
+  handleDelete?: (...args: any) => void,
+  deleteLoading: boolean = false
+): ColumnType<any>[] => [
+  {
+    fixed: 'left',
+    title: 'Назва',
+    dataIndex: 'name',
+    width: '25%',
+  },
+  {
+    title: 'Адміністратори',
+    dataIndex: 'adminEmails',
+    render: (adminEmails) =>
+      adminEmails.map((email) => (
+        <>
+          <span>{email}</span>
+          <br />
+        </>
+      )),
+  },
+  {
+    align: 'center',
+    fixed: 'right',
+    title: 'Опис',
+    dataIndex: 'description',
+    width: 50,
+    render: (text) => (
+      <Tooltip title={text}>
+        <QuestionCircleOutlined />
+      </Tooltip>
+    ),
+  },
+  {
+    align: 'center',
+    fixed: 'right',
+    title: '',
+    dataIndex: '',
+    width: 50,
+    render: (_, domain: IExtendedDomain) => (
+      <Popconfirm
+        title={`Ви впевнені що хочете видалити ${domain.name ?? 'цей домен'}?`}
+        onConfirm={() => handleDelete(domain?._id)}
+        cancelText="Відміна"
+        disabled={deleteLoading}
+      >
+        <DeleteOutlined />
+      </Popconfirm>
+    ),
+  },
+]
+
+export default DomainsTable
