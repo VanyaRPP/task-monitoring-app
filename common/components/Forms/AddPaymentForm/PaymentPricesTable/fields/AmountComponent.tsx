2f18f8e5f39a6fa162d497645de81e5687e7274b
Mykola Yershov <10791500+myershov@users.noreply.github.com>
2023-09-14 20:50:03 +0300

    updated garbage (#403)
    * updated garbage
    
    * fix

---
+++ common/components/Forms/AddPaymentForm/PaymentPricesTable/fields/AmountComponent.tsx
@@ -0,0 +1,29 @@
+import { ServiceType } from '@utils/constants'
+import {
+  AmountGarbageCollectorField,
+  AmountTotalAreaField,
+  AmountPlacingField,
+  AmountInflicionField,
+  AmountElectricityField,
+  AmountWaterField,
+  AmountWaterPartField,
+  AmountDiscountField,
+} from './amountFields'
+
+const fields: any = {
+  [ServiceType.Maintenance]: AmountTotalAreaField,
+  [ServiceType.Placing]: AmountPlacingField,
+  [ServiceType.Electricity]: AmountElectricityField,
+  [ServiceType.Water]: AmountWaterField,
+  [ServiceType.GarbageCollector]: AmountGarbageCollectorField,
+  [ServiceType.Inflicion]: AmountInflicionField,
+  [ServiceType.WaterPart]: AmountWaterPartField,
+  [ServiceType.Discount]: AmountDiscountField,
+}
+
+export default function AmountComponent({ record, edit }) {
+  if (record.name in fields) {
+    const Component = fields[record.name]
+    return <Component record={record} edit={edit} />
+  }
+}
