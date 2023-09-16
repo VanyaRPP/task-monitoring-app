eab5146cf16a2ce393399b862298a1dda20d7fd3
Mykola <my.yershov@gmail.com>
2023-09-16 17:12:37 +0300

    added filter

--- pages/api/spacehub/payment/index.ts
+++ pages/api/spacehub/payment/index.ts
@@ -99,9 +99,7 @@ export default async function handler(
 
         const expr = filterPeriodOptions(req.query)
         if (expr.length > 0) {
-          options.$expr = {
-            $and: expr,
-          }
+          options.$expr = { $and: expr }
         }
 
         const payments = await (Payment as any)
