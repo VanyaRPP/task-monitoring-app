e1e5045bbe1be9f0710b31ece50a978a8fc29e8e
Sigma-UP <mynametrump@gmail.com>
2023-09-25 00:34:03 +0300

    fix: additional tests

--- utils/multiplyFloat.test.ts
+++ utils/multiplyFloat.test.ts
@@ -7,8 +7,11 @@ describe('Multiply float numbers and format result to .xx', () => {
     expect(multiplyFloat((1/2), 0.2)).toBe(0.1)
     expect(multiplyFloat('0.144', '0.2')).toBe(0.02)
     expect(multiplyFloat('0.144', 0.2)).toBe(0.02)
-    expect(multiplyFloat(0.144, 0.2)).toBe(0.02)
-
+    //format
+    expect(multiplyFloat(1, 2)).toBe(2)
+    expect(multiplyFloat(1, 2.5)).toBe(2.5)
+    expect(multiplyFloat(1, 2.05)).toBe(2.05)
+    expect(multiplyFloat(1, 2.005)).toBe(2)
   })
 
   test('if unappropriate value', () => {
