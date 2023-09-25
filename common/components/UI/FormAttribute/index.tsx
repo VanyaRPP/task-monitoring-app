dba6ad641d009c537ea31ea92ee1e45aba4b93b1
Alexey <62567341+bezsliozniy@users.noreply.github.com>
2023-09-25 15:35:50 +0300

    fix: float format for common fields (#418)
    * fix: Water Total format fix
    
    * fix: table data formatting and multiplication algorythm
    
    * fix: additional tests
    
    * fix: column inflictionprice fix and float plus func
    
    * fix: inflictionPricePlacing show fix
    
    * fix: common attributes fix

--- common/components/UI/FormAttribute/index.tsx
+++ common/components/UI/FormAttribute/index.tsx
@@ -1,4 +1,5 @@
 import { useInvoicesPaymentContext } from '@common/components/DashboardPage/blocks/paymentsBulk'
+import { parseStringToFloat } from '@utils/helpers'
 import { Form, FormInstance, Input } from 'antd'
 import { NamePath } from 'antd/lib/form/interface'
 import { useEffect } from 'react'
@@ -6,12 +7,13 @@ import { useEffect } from 'react'
 const FormAttribute: React.FC<{
   value: any
   name: NamePath
-  disabled?: boolean
-}> = ({ value, name, disabled }) => {
+  disabled?: boolean,
+  notNum?: boolean,
+}> = ({ value, name, disabled, notNum  }) => {
   const { form }: { form: FormInstance } = useInvoicesPaymentContext()
 
   useEffect(() => {
-    form.setFieldValue(name, value)
+    form.setFieldValue(name, notNum ? value : +parseStringToFloat(`${value}`))
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [value])
 
