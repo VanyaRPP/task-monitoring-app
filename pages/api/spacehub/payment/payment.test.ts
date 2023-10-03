a9fd383c14d927fd5f2328b1c3d0d5512c740fdc
Rukosmotrov <105110437+Rukosmotrov@users.noreply.github.com>
2023-10-03 19:12:48 +0200

    Payments companies count (#426)
    * Payments companies test
    
    * upd
    
    * Fixed payments backend for distinct companies

--- pages/api/spacehub/payment/payment.test.ts
+++ pages/api/spacehub/payment/payment.test.ts
@@ -324,6 +324,30 @@ describe('Payments API - GET', () => {
   })
 })
 
+it('get valid distinctCompanies as DomainAdmin', async () => {
+  await mockLoginAs(users.domainAdmin)
+
+  const mockReq = {
+    method: 'GET',
+    query: {},
+  } as any
+  const mockRes = {
+    status: jest.fn(() => mockRes),
+    json: jest.fn(),
+  } as any
+
+  await handler(mockReq, mockRes)
+
+  const response = {
+    status: mockRes.status,
+    data: mockRes.json.mock.lastCall[0].data,
+    realEstatesFilter: mockRes.json.mock.lastCall[0].realEstatesFilter,
+  }
+
+  expect(response.status).toHaveBeenCalledWith(200)
+  expect(response.realEstatesFilter[0].value.toString()).toEqual(realEstates[0]._id)
+})
+
 // it('load payments as GlobalAdmin by domainId - success', async () => {
 //   await mockLoginAs(users.globalAdmin)
 
