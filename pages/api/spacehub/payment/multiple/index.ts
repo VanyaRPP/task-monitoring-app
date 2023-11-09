5d2796bb7985f598dea0097207cc38feb0b7352c
Sigma-UP <mynametrump@gmail.com>
2023-11-09 03:31:58 +0200

    feat: multiple payments deletion

---
+++ pages/api/spacehub/payment/multiple/index.ts
@@ -0,0 +1,42 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import Payment from '@common/modules/models/Payment'
+import start, { ExtendedData } from '@pages/api/api.config'
+
+start()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ExtendedData>
+) {
+  switch (req.method) {
+    case 'DELETE':
+      try {
+        const { ids } = req.body;
+
+        if (!ids || !Array.isArray(ids) || ids.length === 0) {
+          return res.status(400).json({
+            success: false,
+            data: 'Invalid or empty list of payment IDs',
+          });
+        }
+
+        const result = await Payment.deleteMany({ _id: { $in: ids } });
+
+        if (result.deletedCount > 0) {
+          return res.status(200).json({
+            success: true,
+            data: `${result.deletedCount} payment(s) were deleted`,
+          });
+        } else {
+          return res.status(400).json({
+            success: false,
+            data: 'No payments were found or deleted',
+          });
+        }
+      } catch (error) {
+        return res.status(500).json({ success: false, error: error.message });
+      }
+    default:
+      res.status(405).end();
+  }
+}
