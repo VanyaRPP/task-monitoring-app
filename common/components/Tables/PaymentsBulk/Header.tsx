c0f56f3bf58cd51a9e4deaf741d5e820436d9022
Alexey <62567341+bezsliozniy@users.noreply.github.com>
2023-11-24 00:13:24 +0200

    feat: MZK field for multiple invoices (#449)

--- common/components/Tables/PaymentsBulk/Header.tsx
+++ common/components/Tables/PaymentsBulk/Header.tsx
@@ -148,6 +148,13 @@ const validateInvoice = (invoice, service) => {
     }
   }
 
+  if (invoice.publicElectricUtility.sum > 0) {
+    result.publicElectricUtilityPrice = {
+      amount: invoice.publicElectricUtility.amount,
+      sum: invoice.publicElectricUtility.sum,
+    }
+  }
+
   if (invoice.cleaningPrice > 0) {
     result.cleaningPrice = {
       price: invoice.cleaningPrice,
