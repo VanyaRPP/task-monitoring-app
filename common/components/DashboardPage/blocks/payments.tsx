27414cd419847b69b9d7d144594b14e0d05d575c
Даниил Овсянников <76049207+DaniilOvsyanniukov@users.noreply.github.com>
2023-12-08 18:46:31 +0200

    Debit&Credit total fix

--- common/components/DashboardPage/blocks/payments.tsx
+++ common/components/DashboardPage/blocks/payments.tsx
@@ -267,36 +267,46 @@ const PaymentsBlock = () => {
       payments?.data && (
         <Table.Summary fixed>
           <Table.Summary.Row className={s.summ_item}>
-            {columns.map((item) => (
+            {columns.map((item, index) => { 
+              let dataindex
+              currUser?.roles?.includes(Roles.GLOBAL_ADMIN)
+              ? dataindex=columns[index-1]?.dataIndex
+              : dataindex=item.dataIndex
+              return(
               <Table.Summary.Cell
                 index={0}
                 key={item.dataIndex}
                 colSpan={item.dataIndex === '' ? 2 : 1}
               >
-                {item.dataIndex === Operations.Debit
+                {dataindex === Operations.Debit
                   ? payments?.totalPayments?.debit?.toFixed(2) || 0
                   : ''}
-                {item.dataIndex === Operations.Credit
+                {dataindex === Operations.Credit
                   ? payments?.totalPayments?.credit?.toFixed(2) || 0
                   : ''}
               </Table.Summary.Cell>
-            ))}
+            )})}
           </Table.Summary.Row>
           <Table.Summary.Row className={s.saldo}>
-            {columns.slice(0, columns.length - 1).map((item) => (
-              <Table.Summary.Cell
+            {columns.slice(0, columns.length - 1).map((item, index) => { 
+              let dataindex
+              currUser?.roles?.includes(Roles.GLOBAL_ADMIN)
+              ? dataindex=columns[index-1]?.dataIndex 
+              : dataindex=item.dataIndex
+              return (              <Table.Summary.Cell
                 colSpan={item.dataIndex === Operations.Debit ? 2 : 1}
                 index={0}
                 key={item.dataIndex}
               >
-                {item.dataIndex === Operations.Debit
+                {dataindex === Operations.Debit
                   ? (
                       (payments?.totalPayments?.debit || 0) -
                       (payments?.totalPayments?.credit || 0)
                     )?.toFixed(2)
                   : false}
-              </Table.Summary.Cell>
-            ))}
+              </Table.Summary.Cell>)
+
+              })}
           </Table.Summary.Row>
         </Table.Summary>
       )
