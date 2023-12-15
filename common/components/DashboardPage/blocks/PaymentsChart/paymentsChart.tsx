ab14c21cc2bf25aaea3653353bdc5d53a105d942
Даниил Овсянников <76049207+DaniilOvsyanniukov@users.noreply.github.com>
2023-12-15 23:37:35 +0200

    for check

--- common/components/DashboardPage/blocks/PaymentsChart/paymentsChart.tsx
+++ common/components/DashboardPage/blocks/PaymentsChart/paymentsChart.tsx
@@ -1,11 +1,15 @@
 import PaymentsChartHeader from '@common/components/Tables/PaymentsChart/Header'
 import TableCard from '@common/components/UI/TableCard'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useGetAllPaymentsQuery } from '@common/api/paymentApi/payment.api'
+import { useGetAllRealEstateQuery } from '@common/api/realestateApi/realestate.api'
 import { AppRoutes, Operations } from '@utils/constants'
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
 import { chartConfig, getPaymentsChartData } from './paymentsChartHelper'
+import {
+  useGetDomainsQuery,
+} from '@common/api/domainApi/domain.api'
 
 const PaymentsChart = () => {
   const Line = dynamic(
@@ -14,18 +18,43 @@ const PaymentsChart = () => {
   )
   const router = useRouter()
   const isOnPage = router.pathname === AppRoutes.PAYMENTS_CHARTS
+  const [companyId, setCompanyId] = useState<string>()
+  const [companyName, setCompanyName] = useState<string>()
   const [paymentsLimit, setPaymentsLimit] = useState(isOnPage ? 10 : 5)
+  const { data: domains } = useGetDomainsQuery({})
+  const { data: realEstates, isLoading } = useGetAllRealEstateQuery({ domainId: domains?.[0]._id })
+
+  useEffect(() => {
+    setCompanyId(realEstates?.data[0]._id)
+    setCompanyName(realEstates?.data[0].companyName)
+  }, [isLoading])
+  
   const { data: payments } = useGetAllPaymentsQuery({
     limit: paymentsLimit,
     type: Operations.Debit,
+    companyIds: [companyId]
+  },
+  {
+    skip: !companyId,
   })
   const chartData = { ...chartConfig, data: getPaymentsChartData(payments?.data) } as any
+
+  if(!companyName){
+    return null
+  }
+
   return (
     <TableCard
       title={
         <PaymentsChartHeader
           paymentsLimit={paymentsLimit}
           setPaymentsLimit={setPaymentsLimit}
+          selectValues={payments?.realEstatesFilter.map((item) => ({
+            label: item.text,
+            value: item.value,
+          }))}
+          setCompanyId={setCompanyId}
+          companyName={companyName}
         />
       }
     >
--- common/components/Tables/PaymentsChart/Header.tsx
+++ common/components/Tables/PaymentsChart/Header.tsx
@@ -3,16 +3,24 @@ import { Button, Select } from 'antd'
 import { useRouter } from 'next/router'
 import { AppRoutes } from '@utils/constants'
 import s from './style.module.scss'
+import { Roles } from '@utils/constants'
+import { useGetCurrentUserQuery } from '@common/api/userApi/user.api'
 
 interface Props {
   paymentsLimit: number,
   setPaymentsLimit: (paymentsLimit: number) => void,
+  selectValues: Array<{ label: string; value: string }>,
+  setCompanyId: (domainId: string) => void,
+  companyName: string
 }
 
-const PaymentsChartHeader: React.FC<Props> = ({ paymentsLimit, setPaymentsLimit }) => {
+const PaymentsChartHeader: React.FC<Props> = ({ paymentsLimit, setPaymentsLimit, selectValues, setCompanyId, companyName }) => {
   const router = useRouter()
   const isOnPage = router.pathname === AppRoutes.PAYMENTS_CHARTS
   const options = [10, 20, 30, 40, 50]
+  const { data: userResponse } = useGetCurrentUserQuery()
+  const isGlobalAdmin = userResponse?.roles?.includes(Roles.GLOBAL_ADMIN)
+  const isDomainAdmin = userResponse?.roles?.includes(Roles.DOMAIN_ADMIN)
 
   return (
     <div className={s.chartBlock}>
@@ -33,6 +41,14 @@ const PaymentsChartHeader: React.FC<Props> = ({ paymentsLimit, setPaymentsLimit 
           }))}
           onChange={setPaymentsLimit}
           defaultValue={paymentsLimit}
+        />
+      )}
+      {(isGlobalAdmin || isDomainAdmin) && (
+        <Select
+          defaultValue={companyName}
+          className={s.companySelector}
+          onChange={setCompanyId}
+          options={selectValues}
         />
       )}
     </div>
