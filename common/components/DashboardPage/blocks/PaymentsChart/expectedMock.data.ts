8f28e4a8ed4e6ce0675905283139b575112a3b7f
Yevhenii Biletskyi <91693377+defild9@users.noreply.github.com>
2024-01-14 23:06:03 +0200

    Change file extension and created file with one test (#475)
    * Change file extension and created file with one test
    
    * Changed file extention for formatData from tsx to ts
    
    * Changed file extention for mockData from tsx to ts, and added mock data for test payment chart data
    
    * Created a test to verify correct filtering and matching of payment data by service type and sorting by date
    
    * Split mock data arrays for test and evaluated data into different files

---
+++ common/components/DashboardPage/blocks/PaymentsChart/expectedMock.data.ts
@@ -0,0 +1,48 @@
+export const expectedResultData = [
+    {
+      date: '01-12-2023',
+      value: 50,
+      category: 'Загальна сума',
+    },
+    {
+      date: '02-12-2023',
+      value: 60,
+      category: 'Загальна сума',
+    },
+    {
+      date: '03-12-2023',
+      value: 20,
+      category: 'Загальна сума',
+    },
+  ]
+  
+  export const expectedResultDataForTestBySort = [
+    {
+      date: '04-12-2023',
+      value: 213,
+      category: 'Загальна сума',
+    },
+    {
+      date: '05-12-2023',
+      value: 120,
+      category: 'Загальна сума',
+    },
+    {
+      date: '06-12-2023',
+      value: 89,
+      category: 'Загальна сума',
+    },  
+  ]
+
+  export const expectedDataForTestForCorrectValue = [
+    {
+      date: '07-12-2023',
+      value: 85,
+      category: 'Загальна сума',
+    },
+    {
+      date: '08-12-2023',
+      value: 60,
+      category: 'Загальна сума',
+    },
+  ]
---
+++ common/components/DashboardPage/blocks/PaymentsChart/paymentsChartHelper.test.ts
@@ -0,0 +1,29 @@
+import { expect } from '@jest/globals'
+import { getPaymentsChartData } from './paymentsChartHelper'
+import { IExtendedPayment } from '@common/api/paymentApi/payment.api.types'
+import { ServiceType } from '@utils/constants'
+import {
+  extendedPayment,
+  extendedPaymentsSort,
+  extendedPaymentForTestForCorrectValue,
+} from '../mock.data'
+import {
+  expectedResultData,
+  expectedResultDataForTestBySort,
+  expectedDataForTestForCorrectValue,
+} from './expectedMock.data'
+
+describe('getPaymentsChartData', () => {
+  it('should return empty array', () => {
+    expect(getPaymentsChartData(undefined)).toEqual([])
+    expect(getPaymentsChartData(null)).toEqual([])
+    expect(getPaymentsChartData([])).toEqual([])
+  })
+  it('correctly filters and maps payment data by service type and sorted by date', () => {
+    expect(getPaymentsChartData(extendedPayment)).toEqual(expectedResultData)
+    expect(getPaymentsChartData(extendedPaymentsSort)).toEqual(
+      expectedResultDataForTestBySort
+    )
+    expect(getPaymentsChartData(extendedPaymentForTestForCorrectValue)).toEqual(expectedDataForTestForCorrectValue)
+  })
+})
