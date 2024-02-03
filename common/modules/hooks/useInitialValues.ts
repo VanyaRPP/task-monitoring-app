8eafe89ad0b3cbc32debff50663c57b324387ba1
Yevhenii Biletskyi <biletskijzena@protonmail.com>
2024-02-04 00:26:05 +0200

    Moved the useInitialValues hook to a separate file

---
+++ common/modules/hooks/useInitialValues.ts
@@ -0,0 +1,24 @@
+import moment from 'moment'
+
+function useInitialValues(currentService) {
+  // TODO: add useEffect || useCallback ?
+  // currently we have few renders
+  // we need it only once. on didmount (first render)
+  const initialValues = {
+    domain: currentService?.domain?.name,
+    street:
+      currentService?.street &&
+      `${currentService.street.address} (м. ${currentService.street.city})`,
+    date: moment(currentService?.date),
+    electricityPrice: currentService?.electricityPrice,
+    inflicionPrice: currentService?.inflicionPrice,
+    rentPrice: currentService?.rentPrice,
+    waterPrice: currentService?.waterPrice,
+    description: currentService?.description,
+    waterPriceTotal: currentService?.waterPriceTotal,
+    garbageCollectorPrice: currentService?.garbageCollectorPrice,
+  }
+  return initialValues
+}
+
+export default useInitialValues
