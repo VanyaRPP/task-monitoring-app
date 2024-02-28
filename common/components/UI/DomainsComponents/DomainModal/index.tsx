16d11bdcd6273eee0ee5ee144034369991706c87
MentoJl <vladganing32@gmail.com>
2024-02-28 23:34:52 +0200

    Info adress error

--- common/components/UI/DomainsComponents/DomainModal/index.tsx
+++ common/components/UI/DomainsComponents/DomainModal/index.tsx
@@ -18,6 +18,7 @@ interface Props {
 
 const DomainModal: FC<Props> = ({ currentDomain, closeModal }) => {
   const [form] = Form.useForm()
+  let errorType = ('')
   const [addDomainEstate] = useAddDomainMutation()
   const [editDomain] = useEditDomainMutation()
 
@@ -33,6 +34,12 @@ const DomainModal: FC<Props> = ({ currentDomain, closeModal }) => {
       description: formData.description,
     }
 
+    if(!domainData.streets.some((i: any) => i.value))
+      errorType = 'adress'
+    // else if
+    else 
+      errorType = ''
+
     const response = currentDomain
       ? await editDomain({
           _id: currentDomain?._id,
@@ -43,11 +50,16 @@ const DomainModal: FC<Props> = ({ currentDomain, closeModal }) => {
     if ('data' in response) {
       form.resetFields()
       closeModal()
+      
       const action = currentDomain ? 'Збережено' : 'Додано'
       message.success(action)
-    } else {
+    } else { 
       const action = currentDomain ? 'збереженні' : 'додаванні'
-      message.error(`Помилка при ${action} надавача послуг`)
+      if(errorType === 'adress') 
+        message.error(`Адреса не знайдена`)
+      // else if
+      else 
+        message.error(`Помилка при ${action} надавача послуг`)
     }
   }
 
