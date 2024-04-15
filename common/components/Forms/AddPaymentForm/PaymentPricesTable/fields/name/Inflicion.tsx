7243bfc7dd92525dd4a3f79cfdd9cdd49e28b4c9
VakotUA <vakotua.g@gmail.com>
2024-04-15 20:19:14 +0300

    Inflicion name

--- common/components/Forms/AddPaymentForm/PaymentPricesTable/fields/name/Inflicion.tsx
+++ common/components/Forms/AddPaymentForm/PaymentPricesTable/fields/name/Inflicion.tsx
@@ -1,10 +1,37 @@
+import { QuestionCircleOutlined } from '@ant-design/icons'
 import { IPaymentField } from '@common/api/paymentApi/payment.api.types'
+import { usePaymentContext } from '@common/components/AddPaymentModal'
+import { inflicionDescription } from '@utils/constants'
+import { Tooltip } from 'antd'
+import s from './style.module.scss'
 
 const Inflicion: React.FC<{
   record: IPaymentField & { key: string }
   preview?: boolean
 }> = ({ record, preview }) => {
-  return <>Індекс інфляції</>
+  const { company, service } = usePaymentContext()
+
+  return (
+    <div className={s.Cell}>
+      {preview ? (
+        <>Індекс інфляції {(service?.inflicionPrice || 0).toFixed(2)}%</>
+      ) : (
+        <Tooltip title={inflicionDescription}>
+          Індекс інфляції {(service?.inflicionPrice || 0).toFixed(2)}%{' '}
+          <QuestionCircleOutlined />
+        </Tooltip>
+      )}
+
+      <span className={s.Sub}>
+        {company?.inflicion &&
+          (+service?.inflicionPrice < 100
+            ? 'Спостерігається дефляція'
+            : +service?.inflicionPrice > 100
+            ? 'Донарахування'
+            : 'Значення незмінне')}
+      </span>
+    </div>
+  )
 }
 
 export default Inflicion
