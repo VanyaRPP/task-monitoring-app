161ea3ade9b74514f2b57d7daa9185aa0d2ca567
VakotUA <vakotua.g@gmail.com>
2024-04-15 21:00:16 +0300

    update Inflicion name
    fix: inflicion style
    tooltip style

--- common/components/Forms/AddPaymentForm/PaymentPricesTable/fields/name/Inflicion.tsx
+++ common/components/Forms/AddPaymentForm/PaymentPricesTable/fields/name/Inflicion.tsx
@@ -2,6 +2,7 @@ import { QuestionCircleOutlined } from '@ant-design/icons'
 import { IPaymentField } from '@common/api/paymentApi/payment.api.types'
 import { usePaymentContext } from '@common/components/AddPaymentModal'
 import { inflicionDescription } from '@utils/constants'
+import { getFormattedDate } from '@utils/helpers'
 import { Tooltip } from 'antd'
 import s from './style.module.scss'
 
@@ -9,26 +10,36 @@ const Inflicion: React.FC<{
   record: IPaymentField & { key: string }
   preview?: boolean
 }> = ({ record, preview }) => {
-  const { company, service } = usePaymentContext()
+  const { company, service, prevPayment } = usePaymentContext()
+
+  const label = prevPayment?.monthService ? (
+    <span className={s.Sub}>
+      {/* eslint-disable-next-line */}
+      {/* @ts-ignore */}
+      {getFormattedDate(prevPayment.monthService.date)}{' '}
+      {(service?.inflicionPrice || 100).toFixed(2)}%
+    </span>
+  ) : null
 
   return (
     <div className={s.Cell}>
-      {preview ? (
-        <>Індекс інфляції {(service?.inflicionPrice || 0).toFixed(2)}%</>
+      Індекс інфляції
+      {preview && label ? (
+        label
       ) : (
-        <Tooltip title={inflicionDescription}>
-          Індекс інфляції {(service?.inflicionPrice || 0).toFixed(2)}%{' '}
-          <QuestionCircleOutlined />
+        <Tooltip className={s.Tooltip} title={inflicionDescription}>
+          {label} <QuestionCircleOutlined />
         </Tooltip>
       )}
-
       <span className={s.Sub}>
         {company?.inflicion &&
-          (+service?.inflicionPrice < 100
+          (!service || +service.inflicionPrice === 0
+            ? 'Значення незмінне'
+            : +service.inflicionPrice < 100
             ? 'Спостерігається дефляція'
-            : +service?.inflicionPrice > 100
+            : +service.inflicionPrice > 100
             ? 'Донарахування'
-            : 'Значення незмінне')}
+            : null)}
       </span>
     </div>
   )
