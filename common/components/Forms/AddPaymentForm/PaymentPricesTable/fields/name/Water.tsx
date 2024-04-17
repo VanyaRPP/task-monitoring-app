1f6265b45cbb08618d4fc7b06e4439f7af607ff2
Vad11m-vek <55487186+Vad11m-vek@users.noreply.github.com>
2024-04-17 22:48:16 +0300

    Update Water.tsx

--- common/components/Forms/AddPaymentForm/PaymentPricesTable/fields/name/Water.tsx
+++ common/components/Forms/AddPaymentForm/PaymentPricesTable/fields/name/Water.tsx
@@ -2,7 +2,7 @@ import { IPaymentField } from '@common/api/paymentApi/payment.api.types'
 import { usePaymentContext } from '@common/components/AddPaymentModal'
 import { getFormattedDate } from '@utils/helpers'
 import s from './style.module.scss'
-
+ 
 const Water: React.FC<{
   record: IPaymentField & { key: string }
   preview?: boolean
