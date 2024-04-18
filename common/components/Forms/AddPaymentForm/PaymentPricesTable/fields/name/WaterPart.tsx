8eaaf57fd8c3da55045457e6f399437f1c9de4f0
Vad11m-vek <55487186+Vad11m-vek@users.noreply.github.com>
2024-04-18 14:51:33 +0300

    payment-prices-table-WaterPart
    payment creation form - "Debit" table

---
+++ common/components/Forms/AddPaymentForm/PaymentPricesTable/fields/name/WaterPart.tsx
@@ -0,0 +1,20 @@
+import { IPaymentField } from '@common/api/paymentApi/payment.api.types'
+import { usePaymentContext } from '@common/components/AddPaymentModal'
+import { getFormattedDate } from '@utils/helpers'
+import s from './style.module.scss'
+
+const WaterPart: React.FC<{
+  record: IPaymentField & { key: string }
+  preview?: boolean
+}> = ({ record, preview }) => {
+	const { service } = usePaymentContext()
+	return (
+		<div className={s.Cell}>
+		  Частка Водопостачання
+		  <span className={s.Sub}>{getFormattedDate(service?.date)}</span>
+		</div>
+	 )
+
+}
+
+export default WaterPart
