20e96c81ab85b265ab33dd411eecad68c2ed8c62
MentoJl <vladganing32@gmail.com>
2024-07-08 17:33:40 +0300

    Update cols showing

--- utils/inflicion.tsx
+++ utils/inflicion.tsx
@@ -1,30 +1,29 @@
 import moment from 'moment'
+import Tooltip from 'antd/es/tooltip'
+import { QuestionCircleOutlined } from '@ant-design/icons'
 
 export function InflicionIndexTitle({ previousMonth }) {
   return (
     <>
       Індекс інфляції
-      {previousMonth ? (
-        <>
-          {' '}
-          {previousMonth?.date ? (
-            <>
-              <br />
-              <b>{moment(previousMonth?.date)?.format('MMMM')}</b>
-            </>
-          ) : (
-            ''
-          )}{' '}
-          {previousMonth?.inflicionPrice !== undefined
-            ? previousMonth?.inflicionPrice?.toFixed(2) + '%'
-            : 'невідомий'}
-        </>
-      ) : (
-        <>
+      {previousMonth?.inflicionPrice !== undefined
+        ? <>
+            <br />
+            {previousMonth?.inflicionPrice?.toFixed(2) + '%'}
+            {' '} 
+            <Tooltip 
+            title={`за ${moment(previousMonth?.date)?.format('MMMM')}`}>
+              <QuestionCircleOutlined />
+            </Tooltip>
+          </>
+        : <>
           <br />
-          за попередній місяць невідомий
-        </>
-      )}
+          {'0% '}
+          <Tooltip 
+          title={'невідомий'}>
+            <QuestionCircleOutlined />
+          </Tooltip>
+      </>}
     </>
   )
 }
