850e16ac7431a75af27445c4b24ec6cb58b00e5b
Yevhenii Biletskyi <biletskijzena@protonmail.com>
2024-07-16 12:51:30 +0300

    Created preview for domains

--- common/components/UI/DomainsComponents/DomainModal/DomainForm/index.tsx
+++ common/components/UI/DomainsComponents/DomainModal/DomainForm/index.tsx
@@ -1,64 +1,60 @@
 import React, { FC } from 'react'
 import { validateField } from '@common/assets/features/validators'
-import { Form, FormInstance, Input, Select } from 'antd'
+import { Form, FormInstance, Input } from 'antd'
 import s from './style.module.scss'
 import DomainStreets from './DomainStreets'
 import EmailSelect from '@common/components/UI/Reusable/EmailSelect'
-import { IExtendedDomain } from '@common/api/domainApi/domain.api.types'
-import { IStreet } from '@common/modules/models/Street'
 
 interface Props {
   form: FormInstance<any>
-  currentDomain: IExtendedDomain
+  editable?: boolean
+  disabled?: boolean
 }
-const DomainForm: FC<Props> = ({ form, currentDomain }) => {
-  const initialValues = useInitialValues(currentDomain)
+
+const DomainForm: FC<Props> = ({ form, editable = true, disabled = false }) => {
+
+  const nameValue = Form.useWatch('name', form)
+  const descriptionValue = Form.useWatch('description', form)
+
   return (
     <Form
       form={form}
       layout="vertical"
       className={s.Form}
-      initialValues={initialValues}
     >
-      <Form.Item name="name" label="Назва" rules={validateField('description')}>
-        <Input
-          placeholder="Вкажіть значення"
-          maxLength={256}
-          className={s.formInput}
-        />
+      <Form.Item name="name" label="Назва" rules={editable && validateField('required')}>
+        {editable ? (
+          <Input
+            placeholder="Вкажіть значення"
+            maxLength={256}
+            className={s.formInput}
+            disabled={disabled}
+          />
+        ) : (
+          <div>{nameValue}</div>
+        )}
       </Form.Item>
-      <EmailSelect form={form} />
-      <DomainStreets />
+      <EmailSelect form={form} disabled={!editable} />
+      <DomainStreets disabled={!editable} />
       <Form.Item
         name="description"
         label="Опис"
-        rules={validateField('required')}
+        rules={editable && validateField('required')}
       >
-        <Input.TextArea
-          placeholder="Вкажіть значення"
-          className={s.formInput}
-          maxLength={512}
-          rows={4}
-        />
+        {editable ? (
+          <Input.TextArea
+            placeholder="Вкажіть значення"
+            className={s.formInput}
+            maxLength={512}
+            rows={4}
+            disabled={disabled}
+          />
+        ) : (
+          <div>{descriptionValue}</div>
+        )}
       </Form.Item>
     </Form>
   )
-}
-
-function useInitialValues(currentDomain: IExtendedDomain) {
-  // TODO: add useEffect || useCallback ?
-  // currently we have few renders
-  // we need it only once. on didmount (first render)
-  const initialValues = {
-    name: currentDomain?.name,
-    adminEmails: currentDomain?.adminEmails,
-    streets: currentDomain?.streets.map((i: any) => ({
-      value: i._id,
-      label: `${i.address} (м. ${i.city})`,
-    })),
-    description: currentDomain?.description,
-  }
-  return initialValues
 }
 
 export default DomainForm
