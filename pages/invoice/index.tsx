fab2567a9d4fb36578f861d1995d8f3d997eb5f5
Serhii Denysiuk <serhiidenysiuk22@gmail.com>
2024-07-19 19:17:35 +0300

    Implemented priceList

--- pages/invoice/index.tsx
+++
@@ -1,9 +0,0 @@
-import withAuthRedirect from "@components/HOC/withAuthRedirect";
-import {FC} from "react";
-import PriceList from "common/components/Forms/AddPaymentForm/PriceList";
-
-
-const Invoices: FC = () => {
-  return <PriceList />
-}
-export default withAuthRedirect(Invoices)
