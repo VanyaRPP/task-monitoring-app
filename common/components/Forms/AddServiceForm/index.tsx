205ef0cbf97ae063bf5b316e391e735be44e76c8
TiFoNi <iitbgtifoni@gmail.com>
2024-07-30 12:13:47 +0300

    Revert "resolve conflicts"
    This reverts commit 4f8055b7ffe556b47ce0a6b466defaf393f90e4a.

--- common/components/Forms/AddServiceForm/index.tsx
+++ common/components/Forms/AddServiceForm/index.tsx
@@ -3,21 +3,10 @@ import { validateField } from '@common/assets/features/validators'
 import AddressesSelect from '@common/components/UI/Reusable/AddressesSelect'
 import DomainsSelect from '@common/components/UI/Reusable/DomainsSelect'
 import { usePreviousMonthService } from '@common/modules/hooks/useService'
-import {
-  DatePicker,
-  Form,
-  FormInstance,
-  Input,
-  InputNumber,
-  ConfigProvider,
-} from 'antd'
+import { DatePicker, Form, FormInstance, Input, InputNumber } from 'antd'
 import dayjs from 'dayjs'
 import { useEffect } from 'react'
 import s from './style.module.scss'
-import 'dayjs/locale/uk'
-import ukUA from 'antd/lib/locale/uk_UA'
-
-dayjs.locale('uk')
 
 interface Props {
   form: FormInstance<any>
@@ -57,94 +46,77 @@ const AddServiceForm: React.FC<Props> = ({ form, edit, currentService }) => {
     })
   }, [form, currentService, previousMonth])
 
-  const initialValues = {
-    domain: currentService?.domain?._id,
-    street: currentService?.street?._id,
-    date: dayjs(currentService?.date),
-    description: currentService?.description,
-  }
-
   return (
-    <ConfigProvider locale={ukUA}>
-      <Form
-        initialValues={initialValues}
-        form={form}
-        layout="vertical"
-        className={s.Form}
+    <Form
+      form={form}
+      layout="vertical"
+      className={s.Form}
+      initialValues={{
+        domain: currentService?.domain?._id,
+        street: currentService?.street?._id,
+        date: dayjs(currentService?.date),
+        description: currentService?.description,
+      }}
+    >
+      <DomainsSelect form={form} edit={edit} />
+      <AddressesSelect form={form} edit={edit} />
+      <Form.Item
+        name="date"
+        label="Місяць та рік"
+        rules={validateField('required')}
       >
-        {edit ? (
-          <>
-            <Form.Item label="Надавач послуг">
-              <Input value={currentService?.domain.name} disabled />
-            </Form.Item>
-            <Form.Item label="Адреса">
-              <Input
-                value={`${currentService?.street?.address}, ${currentService?.street?.city}`}
-                disabled
-              />
-            </Form.Item>
-          </>
-        ) : (
-          <>
-            <DomainsSelect form={form} />
-            <AddressesSelect form={form} />
-          </>
-        )}
-        <Form.Item
-          name="date"
-          label="Місяць та рік"
-          rules={validateField('required')}
-        >
-          <MonthPicker
-            format="MMMM YYYY"
-            placeholder="Оберіть місяць"
-            className={s.formInput}
-          />
-        </Form.Item>
-        <Form.Item
-          name="rentPrice"
-          label="Утримання приміщень (грн/м²)"
-          rules={validateField('required')}
-        >
-          <InputNumber placeholder="Вкажіть значення" className={s.formInput} />
-        </Form.Item>
-        <Form.Item
-          name="electricityPrice"
-          label="Електроенергія (грн/кВт)"
-          rules={validateField('electricityPrice')}
-        >
-          <InputNumber placeholder="Вкажіть значення" className={s.formInput} />
-        </Form.Item>
-        <Form.Item
-          name="waterPrice"
-          label="Водопостачання (грн/м³)"
-          rules={validateField('required')}
-        >
-          <InputNumber placeholder="Вкажіть значення" className={s.formInput} />
-        </Form.Item>
-        <Form.Item
-          name="waterPriceTotal"
-          label="Всього водопостачання (грн/м³)"
-          rules={validateField('required')}
-        >
-          <InputNumber placeholder="Вкажіть значення" className={s.formInput} />
-        </Form.Item>
-        <Form.Item name="garbageCollectorPrice" label="Вивіз сміття">
-          <InputNumber placeholder="Вкажіть значення" className={s.formInput} />
-        </Form.Item>
-        <Form.Item name="inflicionPrice" label="Індекс інфляції">
-          <InputNumber placeholder="Вкажіть значення" className={s.formInput} />
-        </Form.Item>
-        <Form.Item name="description" label="Опис">
-          <Input.TextArea
-            placeholder="Введіть опис"
-            autoSize={{ minRows: 2, maxRows: 5 }}
-            maxLength={256}
-            className={s.formInput}
-          />
-        </Form.Item>
-      </Form>
-    </ConfigProvider>
+        <MonthPicker
+          format="MMMM YYYY"
+          placeholder="Оберіть місяць"
+          className={s.formInput}
+        />
+      </Form.Item>
+      <Form.Item
+        name="rentPrice"
+        label="Утримання приміщень (грн/м²)"
+        rules={validateField('required')}
+      >
+        <InputNumber placeholder="Вкажіть значення" className={s.formInput} />
+      </Form.Item>
+      <Form.Item
+        name="electricityPrice"
+        label="Електроенергія (грн/кВт)"
+        rules={validateField('electricityPrice')}
+      >
+        <InputNumber placeholder="Вкажіть значення" className={s.formInput} />
+      </Form.Item>
+      <Form.Item
+        name="waterPrice"
+        label="Водопостачання (грн/м³)"
+        rules={validateField('required')}
+      >
+        <InputNumber placeholder="Вкажіть значення" className={s.formInput} />
+      </Form.Item>
+      <Form.Item
+        name="waterPriceTotal"
+        label="Всього водопостачання (грн/м³)"
+        rules={validateField('required')}
+      >
+        <InputNumber placeholder="Вкажіть значення" className={s.formInput} />
+      </Form.Item>
+      <Form.Item name="garbageCollectorPrice" label="Вивіз сміття">
+        <InputNumber placeholder="Вкажіть значення" className={s.formInput} />
+      </Form.Item>
+      <Form.Item name="inflicionPrice" label="Індекс інфляції">
+        <InputNumber placeholder="Вкажіть значення" className={s.formInput} />
+      </Form.Item>
+      <Form.Item name="description" label="Опис">
+        <Input.TextArea
+          placeholder="Введіть опис"
+          autoSize={{
+            minRows: 2,
+            maxRows: 5,
+          }}
+          maxLength={256}
+          className={s.formInput}
+        />
+      </Form.Item>
+    </Form>
   )
 }
 
