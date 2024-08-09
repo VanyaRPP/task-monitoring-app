393a48e05d524ee01e848e95aa5603a7e3d85233
vakot <vakotua.g@gmail.com>
2024-08-09 14:13:46 +0300

    advanced theme
    - sync theme with localStorage
    - initial theme from browser preferences

--- common/components/UI/ThemeSwitcher/index.tsx
+++ common/components/UI/ThemeSwitcher/index.tsx
@@ -1,18 +1,20 @@
 import { SunFilled, SunOutlined } from '@ant-design/icons'
 import useTheme from '@modules/hooks/useTheme'
-import { Button } from 'antd'
+import { Button, Tooltip } from 'antd'
 import React from 'react'
 
 const ThemeSwitcher: React.FC = () => {
   const [theme, setTheme] = useTheme()
 
   return (
-    <Button
-      icon={theme === 'light' ? <SunFilled /> : <SunOutlined />}
-      size="large"
-      shape="circle"
-      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-    />
+    <Tooltip title={`Switch theme to ${theme === 'light' ? 'dark' : 'light'}`}>
+      <Button
+        icon={theme === 'light' ? <SunFilled /> : <SunOutlined />}
+        size="large"
+        shape="circle"
+        onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      />
+    </Tooltip>
   )
 }
 
--- common/modules/hooks/useTheme.ts
+++ common/modules/hooks/useTheme.ts
@@ -1,16 +1,35 @@
 import { RootState } from '@modules/store/store'
 import { setTheme as _setTheme, Theme } from '@modules/store/themeSlice'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+
+const getInitialTheme = (): Theme => {
+  return window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? 'dark'
+    : 'light'
+}
 
 const useTheme = (): [Theme, (theme: Theme) => void] => {
   const theme = useSelector((state: RootState) => state.theme)
   const dispatch = useDispatch()
 
   const setTheme = useCallback(
-    (theme: Theme) => dispatch(_setTheme(theme)),
+    (theme: Theme) => {
+      localStorage.setItem('theme', theme)
+      dispatch(_setTheme(theme))
+    },
     [dispatch]
   )
+
+  useEffect(() => {
+    const _theme = localStorage.getItem('theme') as Theme
+
+    if (_theme) {
+      dispatch(_setTheme(_theme))
+    } else {
+      setTheme(getInitialTheme())
+    }
+  }, [dispatch, setTheme])
 
   return [theme, setTheme]
 }
