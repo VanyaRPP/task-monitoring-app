b8b6b0f30af0cb0deeb5606ab8ae9abf09e203ed
Mykola Yershov <vanassssssssss@gmail.com>
2024-08-21 14:20:30 +0300

    add iban validation

--- common/components/UI/DomainsComponents/DomainModal/DomainForm/index.tsx
+++ common/components/UI/DomainsComponents/DomainModal/DomainForm/index.tsx
@@ -1,6 +1,6 @@
 import { validateField } from '@assets/features/validators'
 import EmailSelect from '@components/UI/Reusable/EmailSelect'
-import { Form, FormInstance, Input } from 'antd'
+import { Form, FormInstance, Input, Select, Space } from 'antd'
 import { FC } from 'react'
 import DomainStreets from './DomainStreets'
 import s from './style.module.scss'
@@ -16,6 +16,16 @@ const DomainForm: FC<Props> = ({
   editable = true,
   setIsValueChanged,
 }) => {
+  const options = [
+    {
+      value: 'FOP',
+      label: 'FOP',
+    },
+    {
+      value: 'TOV',
+      label: 'TOV',
+    },
+  ]
   return (
     <Form
       form={form}
@@ -34,16 +44,53 @@ const DomainForm: FC<Props> = ({
       </Form.Item>
       <EmailSelect form={form} disabled={!editable} />
       <DomainStreets disabled={!editable} />
-      <Form.Item
-        name="description"
-        label="Опис"
-        rules={validateField('required')}
-      >
+      {/* individual entrepreneur | FOP */}
+      <Form.Item name="IE" label="FOP" rules={validateField('required')}>
+        <Space.Compact className={s.formInput}>
+          <Select
+            style={{ width: '20%' }}
+            defaultValue="FOP"
+            options={options}
+          />
+          <Input
+            placeholder="Вкажіть ФОП"
+            maxLength={256}
+            className={s.formInput}
+            disabled={!editable}
+          />
+        </Space.Compact>
+      </Form.Item>
+      {/* IBAN */}
+      <Form.Item name="IBAN" label="IBAN" rules={validateField('IBAN')}>
+        <Input
+          placeholder="Вкажіть IBAN"
+          maxLength={34}
+          className={s.formInput}
+          disabled={!editable}
+        />
+      </Form.Item>
+      <Form.Item name="MFO" label="МФО" rules={validateField('required')}>
+        <Input
+          placeholder="Вкажіть МФО"
+          maxLength={256}
+          className={s.formInput}
+          disabled={!editable}
+        />
+      </Form.Item>
+      <Form.Item name="description" label="Опис">
         <Input.TextArea
           placeholder="Вкажіть значення"
           className={s.formInput}
           maxLength={512}
           rows={4}
+          disabled={!editable}
+        />
+      </Form.Item>
+
+      <Form.Item name="PRIVATE_TOKEN" label="Private Token">
+        <Input
+          placeholder="Token"
+          className={s.formInput}
           disabled={!editable}
         />
       </Form.Item>
