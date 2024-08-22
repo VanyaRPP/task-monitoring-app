007ff2b84da8d354a554714aec7b36b79ba6a416
djproblematic <djproblematicbusiness@gmail.com>
2024-08-23 00:54:25 +0300

    mini profile blur

--- common/components/UI/Avatar/index.tsx
+++ common/components/UI/Avatar/index.tsx
@@ -2,34 +2,25 @@
 
 import { UserOutlined } from '@ant-design/icons'
 import { Profile } from '@components/UI/Profile'
-import { isEmpty } from '@utils/helpers'
 import { Avatar as AntdAvatar, Popover } from 'antd'
 import { useSession } from 'next-auth/react'
 import Image from 'next/image'
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export interface AvatarProps {
   size?: number
-  open?: boolean
   onOpen?: () => void
   onClose?: () => void
 }
 
 export const Avatar: React.FC<AvatarProps> = ({
   size = 40,
-  open: _open,
   onOpen,
   onClose,
 }) => {
   const { data: session } = useSession()
 
   const [open, setOpen] = useState<boolean>(false)
-
-  const handleClick = useCallback(() => {
-    if (isEmpty(_open)) {
-      setOpen(!open)
-    }
-  }, [open, _open, setOpen])
 
   useEffect(() => {
     if (open) {
@@ -39,18 +30,16 @@ export const Avatar: React.FC<AvatarProps> = ({
     }
   }, [open, onOpen, onClose])
 
-  useEffect(() => {
-    if (!isEmpty(_open)) {
-      setOpen(!!_open)
-    }
-  }, [_open, setOpen])
-
   return (
-    <Popover open={open} placement="bottomRight" content={<Profile />}>
+    <Popover
+      trigger="click"
+      onOpenChange={setOpen}
+      placement="bottomRight"
+      content={<Profile />}
+    >
       <AntdAvatar
         size={size}
         icon={<UserOutlined />}
-        onClick={handleClick}
         src={
           session?.user?.image ? (
             <Image
