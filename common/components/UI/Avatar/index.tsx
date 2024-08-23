823e30f05378a42f64902960ad6b853f95459de6
vakot <vakotua.g@gmail.com>
2024-08-23 14:46:07 +0300

    useCallback instead of useState & useEffect

--- common/components/UI/Avatar/index.tsx
+++ common/components/UI/Avatar/index.tsx
@@ -5,35 +5,39 @@ import { Profile } from '@components/UI/Profile'
 import { Avatar as AntdAvatar, Popover } from 'antd'
 import { useSession } from 'next-auth/react'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import { useCallback } from 'react'
 
 export interface AvatarProps {
   size?: number
   onOpen?: () => void
   onClose?: () => void
+  onChange?: (open: boolean) => void
 }
 
 export const Avatar: React.FC<AvatarProps> = ({
   size = 40,
   onOpen,
   onClose,
+  onChange,
 }) => {
   const { data: session } = useSession()
 
-  const [open, setOpen] = useState<boolean>(false)
-
-  useEffect(() => {
-    if (open) {
-      onOpen?.()
-    } else {
-      onClose?.()
-    }
-  }, [open, onOpen, onClose])
+  const handleChange = useCallback(
+    (newOpen: boolean) => {
+      if (newOpen) {
+        onOpen?.()
+      } else {
+        onClose?.()
+      }
+      onChange?.(newOpen)
+    },
+    [onClose, onOpen, onChange]
+  )
 
   return (
     <Popover
       trigger="click"
-      onOpenChange={setOpen}
+      onOpenChange={handleChange}
       placement="bottomRight"
       content={<Profile />}
     >
