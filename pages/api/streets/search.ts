85bb62f1ca510a29c7d796b15dcab2608098b756
Serhii Denysiuk <serhiidenysiuk22@gmail.com>
2024-09-17 12:39:58 +0300

    Added autocomplete to creating address

---
+++ pages/api/streets/search.ts
@@ -0,0 +1,26 @@
+import Street from "@modules/models/Street";
+import {NextApiRequest, NextApiResponse} from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case 'GET':
+      try {
+        const { city, address } = req.query;
+
+        if (!city || !address) {
+          return res.status(400).json({ success: false, message: 'City and address are required' });
+        }
+        const streets = await Street.find({
+          city: new RegExp(city as string, 'i'),
+          address: new RegExp(address as string, 'i')
+        }).limit(10);
+
+        return res.status(200).json({ success: true, data: streets });
+      } catch (error) {
+        return res.status(500).json({ success: false, message: 'Server error' });
+      }
+
+    default:
+      return res.status(405).json({ success: false, message: 'Method not allowed' });
+  }
+}
