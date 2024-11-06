6f7c30815732de55cb8fa5f3ab889c7f97097379
Andrii Novakovskyi <141185356+oniguy4649@users.noreply.github.com>
2024-11-06 20:23:13 +0200

    Adjust color for better contrast and add empty line

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,11 +5,14 @@ import type { DocumentContext } from 'next/document'
 
 const MyDocument = () => (
   <Html lang="uk">
-      <Head>
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-          <meta name="theme-color" content="#6633cc" />
-      </Head>
+    <Head>
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
+      <meta name="theme-color" content="#783fc4" />
+    </Head>
     <body>
       <Main />
       <NextScript />
