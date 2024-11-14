dae339ab239d3b82c3523c0ca626b241a3e55f0e
MentoJl <vladganing32@gmail.com>
2024-11-14 18:56:51 +0200

    fixed

--- common/components/Tables/EditInvoiceTable/Placing/index.tsx
+++ common/components/Tables/EditInvoiceTable/Placing/index.tsx
@@ -43,12 +43,13 @@ export const Amount: React.FC<InvoiceComponentProps> = ({
   const name = useMemo(() => toArray<string>(_name), [_name])
 
   const { company, prevService, prevPayment } = usePaymentContext()
-
+  const [initialPrice, setInitialPrice] = useState(null)
   const amount = Form.useWatch(['invoice', ...name, 'amount'], form)
   const price = Form.useWatch(['invoice', ...name, 'price'], form)
+  useEffect(() => {if (price !== undefined && initialPrice === null) setInitialPrice(price)})
   const invoices: InvoiceType[] = Form.useWatch(['invoice'], form)
 
-  const inflicionInvoice: InvoiceType | undefined = useMemo(() => {
+  const inflicionInvoice = useMemo(() => {
     return invoices?.find((invoice) => invoice.type === ServiceType.Inflicion)
   }, [invoices])
 
@@ -86,12 +87,12 @@ export const Amount: React.FC<InvoiceComponentProps> = ({
           </Typography.Text>
         )}
         {!isInitial && editable && (
-          <Tooltip title="Відновити початкове значення">
+          <Tooltip title={`Відновити початкове значення ${inflicionInvoice}`}>
             <Button
               onClick={() =>
                 form.setFieldValue(
                   ['invoice', ...name, 'price'],
-                  +toRoundFixed(rentPrice + inflicionInvoice?.sum)
+                  +initialPrice
                 )
               }
               icon={<ReloadOutlined />}
