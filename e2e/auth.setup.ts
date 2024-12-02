c23b6dae25890a5a2b9501e38e5ae3786d594217
hacer7 <74371518+hacer7@users.noreply.github.com>
2024-12-02 15:54:33 +0200

    Update auth.setup.ts

--- e2e/auth.setup.ts
+++ e2e/auth.setup.ts
@@ -3,7 +3,7 @@ import { test as setup } from '@playwright/test'
 setup('authenticate as global admin', async ({ page }) => {
   const authFile = 'e2e/globalAdmin.json'
 
-  await page.goto('/auth/signin')
+  await page.goto('https://dev.d3lib7r0j7pety.amplifyapp.com/auth/signin')
 
   await page.locator('[data-e2e="authFormEmail"]').fill('globalAdmin@test.com')
   await page.locator('[data-e2e="authFormPassword"]').fill('testGlobalAdmin123')
@@ -17,7 +17,7 @@ setup('authenticate as global admin', async ({ page }) => {
 setup('authenticate as domain admin', async ({ page }) => {
   const authFile = 'e2e/domainAdmin.json'
 
-  await page.goto('/auth/signin')
+  await page.goto('https://dev.d3lib7r0j7pety.amplifyapp.com/auth/signin')
 
   await page.locator('[data-e2e="authFormEmail"]').fill('domainAdmin@test.com')
   await page.locator('[data-e2e="authFormPassword"]').fill('testDomainAdmin123')
