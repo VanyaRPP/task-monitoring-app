f0867c03417ed8228425c39f23a26ad83338ba47
korlak <korvenlakon@gmail.com>
2024-12-05 16:54:01 +0200

    playwright service

--- e2e/common/createServiceForm.ts
+++ e2e/common/createServiceForm.ts
@@ -15,21 +15,20 @@ export const createServiceForm = (page: Page) => {
 
     async fillForm() {
       await page.getByLabel('Надавач послуг').fill('123qwe')
-      await page.locator('.ant-select-item', { hasText: '123qwe' }).click();
+      await page.locator('.ant-select-item', { hasText: '123qwe' }).click()
 
-      await page.getByLabel('Місяць та рік').click();
-      await page.locator('.ant-picker-cell', { hasText: 'Січ' }).click();
+      await page.getByLabel('Місяць та рік').click()
+      await page.locator('.ant-picker-cell', { hasText: 'Січ' }).click()
 
-      await page.getByLabel('Утримання приміщень (грн/м²)').fill('10');
-      await page.getByLabel('Електроенергія (грн/кВт)').fill('20');
-      await page.locator('#waterPrice').fill('15');
-      await page.getByLabel('Всього водопостачання (грн/м³)').fill('25');
-      await page.getByLabel('Вивіз сміття').fill('5');
-      await page.locator('#inflicionPrice').fill('5');
+      await page.getByLabel('Утримання приміщень (грн/м²)').fill('10')
+      await page.getByLabel('Електроенергія (грн/кВт)').fill('20')
+      await page.locator('#waterPrice').fill('15')
+      await page.getByLabel('Всього водопостачання (грн/м³)').fill('25')
+      await page.getByLabel('Вивіз сміття').fill('5')
+      await page.locator('#inflicionPrice').fill('5')
 
-      await page.getByLabel('Опис').fill('тест');
+      await page.getByLabel('Опис').fill('тест')
     },
-
 
     async submitForm() {
       await page.getByRole('button', { name: 'Додати', exact: true }).click()
