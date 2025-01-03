bd5bc2c57190a9f0a53932ea40cf20a08c5dfbfc
Andrii Novakovskyi <141185356+oniguy4649@users.noreply.github.com>
2025-01-03 19:20:46 +0200

    Test to scroll to first invalid field

---
+++ e2e/common/scrollToFirstInvalidField.ts
@@ -0,0 +1,41 @@
+import { expect, Page } from '@playwright/test';
+import routeNavigator from './routerNavigation';
+import { AppRoutes } from '@utils/constants';
+
+export const scrollToFirstInvalidField = (page: Page) => {
+  const goto = routeNavigator(page, AppRoutes);
+
+  return {
+    openForm: async () => {
+      await goto(AppRoutes.PAYMENT_BULK);
+    },
+
+    fillForm: async () => {
+      await page.waitForTimeout(5000);
+
+      await page.getByLabel('Надавач послуг').click();
+
+      // await page.locator('.ant-select[aria-label="Надавач послуг"] .ant-select-selector').click();
+
+      await page.locator('.ant-select-dropdown .ant-select-item').waitFor();
+
+      await page.locator('.ant-select-item', { hasText: 'Pahan domen' }).click();
+
+      await expect(page.locator('.ant-select[aria-label="Надавач послуг"] .ant-select-selector')).toHaveText('Pahan domen');
+    },
+
+    clickSaveButton: async () => {
+      await page.click('button:has-text("Зберегти")');
+    },
+
+    assertErrorDisplayed: async () => {
+      const errorMessageLocator = page.locator('text=Не менше 0');
+      await expect(errorMessageLocator).toBeVisible();
+
+      const fieldWithError = page.locator('#payments_0_invoice_electricityPrice_sum');
+      await expect(fieldWithError).toHaveClass(/ant-input-status-error/);
+
+      await expect(fieldWithError).toBeInViewport();
+    },
+  };
+};
---
+++ e2e/tests/scrollToFirstInvalidField.test.ts
@@ -0,0 +1,20 @@
+import { test } from '@playwright/test';
+import { scrollToFirstInvalidField } from '../common/scrollToFirstInvalidField';
+
+test.describe('Payment Form Validation', () => {
+  test.use({ storageState: 'e2e/domainAdmin.json' })
+
+  test('should scroll to the first invalid field and display an error', async ({ page }) => {
+    const formHelper = scrollToFirstInvalidField(page);
+
+    await page.waitForTimeout(5000);
+
+    await formHelper.openForm();
+
+    await formHelper.fillForm();
+
+    await formHelper.clickSaveButton();
+
+    await formHelper.assertErrorDisplayed();
+  });
+});
