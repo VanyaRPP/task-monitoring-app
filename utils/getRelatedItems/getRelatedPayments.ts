85227e03d72075c6a5e4500b9bb9e9032ef1fbb1
korlak <58148263+korlak@users.noreply.github.com>
2025-01-31 18:54:29 +0300

    relatedItems (#800)
    * relatedItems
    
    * Update index.ts
    
    * Update index.ts
    
    ---------
    
    Co-authored-by: MentoJl <80547023+MentoJl@users.noreply.github.com>

---
+++ utils/getRelatedItems/getRelatedPayments.ts
@@ -0,0 +1,20 @@
+import User from '@modules/models/User'
+import Payment from '@modules/models/Payment'
+import { getRelatedDomains } from './getRelatedDomains'
+import { getRelatedServices } from './getRelatedServices'
+import { getRelatedCompanies } from './getRelatedCompanies'
+
+export async function getRelatedPayments(userId: string) {
+    const { domainIds } = await getRelatedDomains(userId)
+    const { serviceIds } = await getRelatedServices(userId)
+    const { companyIds } = await getRelatedCompanies(userId)
+
+    const paymentsIds = await Payment.distinct('_id', {
+        domain: { $in: domainIds },
+        service: { $in: serviceIds },
+        company: { $in: companyIds },
+    })
+
+    return {paymentsIds}
+
+}    
