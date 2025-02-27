ab74298eaafc5a0c91944438547be6c480e21e98
korlak <58148263+korlak@users.noreply.github.com>
2025-02-27 10:32:28 +0300

    Jest tests profit (#855)
    * test profit
    
    * fix

--- pages/api/profit/profit.test.ts
+++ pages/api/profit/profit.test.ts
@@ -1,189 +1,219 @@
-// import { createMocks } from 'node-mocks-http'
-// import handler from './index'
-// import Domain from '@modules/models/Domain'
-// import Payment from '@modules/models/Payment'
-// import { getCurrentUser } from '@utils/getCurrentUser'
-// import dbConnect from '@utils/dbConnect'
-// import mongoClient from '@common/lib/mongodb'
-// import dayjs from 'dayjs'
-// import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
-// import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
-// dayjs.extend(isSameOrBefore)
-// dayjs.extend(isSameOrAfter)
+import handler from './index'
+import Domain from '@modules/models/Domain'
+import Payment from '@modules/models/Payment'
+import Credit from '@modules/models/Credit'
+import { getCurrentUser } from '@utils/getCurrentUser'
+import dbConnect from '@utils/dbConnect'
+import mongoClient from '@common/lib/mongodb'
+import dayjs from 'dayjs'
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-// jest.mock('@utils/getCurrentUser')
-// jest.mock('@modules/models/Domain')
-// jest.mock('@modules/models/Payment')
-// jest.mock('@utils/dbConnect', () => ({
-//   __esModule: true,
-//   default: jest.fn(),
-// }))
+import {
+  payments,
+  users,
+  realEstates,
+  domains,
+  services,
+} from '@utils/testData'
 
-// jest.mock('@common/lib/mongodb', () => ({
-//   __esModule: true,
-//   default: jest.fn(),
-// }))
+jest.setTimeout(60000)
 
-// // Мокаємо MongoDB адаптер для next-auth
-// jest.mock('@next-auth/mongodb-adapter', () => ({
-//   __esModule: true,
-//   MongoDBAdapter: jest.fn(() => ({
-//     db: jest.fn(() => ({
-//       collection: jest.fn(),
-//     })),
-//   })),
-// }))
+dayjs.extend(isSameOrBefore)
+dayjs.extend(isSameOrAfter)
 
-// describe('Profit Payment API - GET', () => {
-//   beforeEach(() => {
-//     jest.clearAllMocks()
-//   })
+jest.mock('@utils/getCurrentUser')
+jest.mock('@modules/models/Domain')
+jest.mock('@modules/models/Payment')
+jest.mock('@modules/models/Credit', () => ({
+  find: jest.fn(),
+}))
+jest.mock('@utils/dbConnect', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}))
 
-//   it('should return grouped payments for a DomainAdmin and log data', async () => {
-//     ;(getCurrentUser as jest.Mock).mockResolvedValue({
-//       isDomainAdmin: true,
-//       isGlobalAdmin: false,
-//       isAdmin: false,
-//       user: { email: 'admin@example.com' },
-//     })
-//     ;(Domain.find as jest.Mock).mockResolvedValue([{ _id: 'domain1' }])
-//     ;(Payment.find as jest.Mock).mockResolvedValue([
-//       {
-//         invoiceCreationDate: new Date('2024-03-15'),
-//         generalSum: 787006.49,
-//         type: 'debit',
-//       },
-//       {
-//         invoiceCreationDate: new Date('2024-08-10'),
-//         generalSum: 76433,
-//         type: 'credit',
-//       },
-//       {
-//         invoiceCreationDate: new Date('2024-09-01'),
-//         generalSum: 207299.43,
-//         type: 'credit',
-//       },
-//       {
-//         invoiceCreationDate: new Date('2024-09-20'),
-//         generalSum: 2108832.48,
-//         type: 'debit',
-//       },
-//     ])
+jest.mock('@common/lib/mongodb', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}))
 
-//     const { req, res } = createMocks({
-//       method: 'GET',
-//       query: {},
-//     })
+// Мокаємо MongoDB адаптер для next-auth
+jest.mock('@next-auth/mongodb-adapter', () => ({
+  __esModule: true,
+  MongoDBAdapter: jest.fn(() => ({
+    db: jest.fn(() => ({
+      collection: jest.fn(),
+    })),
+  })),
+}))
 
-//     await handler(req, res)
+describe('Profit Payment API - GET', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+  it('should return grouped payments for a DomainAdmin and log data', async () => {
+    ;(getCurrentUser as jest.Mock).mockResolvedValue({
+      isDomainAdmin: true,
+      isGlobalAdmin: false,
+      isAdmin: false,
+      user: { email: 'admin@example.com' },
+    })
+    ;(Domain.find as jest.Mock).mockResolvedValue([{ _id: '66fff715e02a727351b1f403' }])
+    ;(Payment.find as jest.Mock).mockResolvedValue([
+      {
+        invoiceCreationDate: new Date('2024-03-15'),
+        generalSum: 787006.49,
+        type: 'debit',
+      },
+      {
+        invoiceCreationDate: new Date('2024-08-10'),
+        generalSum: 76433,
+        type: 'credit',
+      },
+      {
+        invoiceCreationDate: new Date('2024-09-01'),
+        generalSum: 207299.43,
+        type: 'credit',
+      },
+      {
+        invoiceCreationDate: new Date('2024-09-20'),
+        generalSum: 2108832.48,
+        type: 'debit',
+      },
+    ])
+  
+    ;(Credit.find as jest.Mock).mockResolvedValue([
+      {
+        _id: '66fff715e02a727351b1f404',
+        domain: '66fff715e02a727351b1f403', 
+        date: new Date('2024-06-15'), 
+        sum: 50000,
+      },
+    ])
+  
+    const req = { method: 'GET', query: {} } as NextApiRequest
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as NextApiResponse
+  
+    await handler(req, res)
+  
+    const response = (res.json as jest.Mock).mock.calls[0][0]
+  
+    const currentDate = dayjs()
+    response.data.forEach((entry: any) => {
+      const entryDate = dayjs(entry.month, 'YYYY-MM')
+      expect(entryDate.isSameOrAfter(currentDate.subtract(12, 'month'))).toBe(true)
+      expect(entryDate.isSameOrBefore(currentDate)).toBe(true)
+    })
+  
+    expect(response.success).toBe(true)
+    expect(response.data).toEqual([
+      {
+        totalGeneralSumCredit: 0,
+        totalGeneralSumDebit: 787006.49,
+        month: '2024-03',
+      },
+      {
+        totalGeneralSumCredit: 0,
+        totalGeneralSumDebit: 76433,
+        month: '2024-08',
+      },
+      {
+        totalGeneralSumCredit: 0,
+        totalGeneralSumDebit: 2316131.91,
+        month: '2024-09',
+      },
+      {
+        totalGeneralSumCredit: 50000,
+        totalGeneralSumDebit: 0,
+        month: '2024-06',
+      },
+    ])
+  })
+  
 
-//     expect(res._getStatusCode()).toBe(200)
+  it('should return 200 with "not allowed" message if user is not an admin', async () => {
+    ;(getCurrentUser as jest.Mock).mockResolvedValue({
+      isDomainAdmin: false,
+      isGlobalAdmin: false,
+      isAdmin: false,
+      user: { email: 'user@example.com' },
+    })
 
-//     const response = JSON.parse(res._getData())
+    const req = { method: 'GET', query: {} } as NextApiRequest
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as NextApiResponse
 
-//     const currentDate = dayjs()
-//     response.data.forEach((entry: any) => {
-//       const entryDate = dayjs(entry.month, 'YYYY-MM')
-//       expect(entryDate.isSameOrAfter(currentDate.subtract(12, 'month'))).toBe(
-//         true
-//       )
-//       expect(entryDate.isSameOrBefore(currentDate)).toBe(true)
-//     })
+    await handler(req, res)
 
-//     expect(response.success).toBe(true)
-//     expect(response.data).toEqual([
-//       {
-//         totalGeneralSumCredit: 0,
-//         totalGeneralSumDebit: 787006.49,
-//         month: '2024-03',
-//       },
-//       {
-//         totalGeneralSumCredit: 76433,
-//         totalGeneralSumDebit: 0,
-//         month: '2024-08',
-//       },
-//       {
-//         totalGeneralSumCredit: 207299.43,
-//         totalGeneralSumDebit: 2108832.48,
-//         month: '2024-09',
-//       },
-//     ])
-//   })
+    expect(res.status).toHaveBeenCalledWith(200)
+    const response = (res.json as jest.Mock).mock.calls[0][0]
+    expect(response.message).toEqual('not allowed')
+    expect(response.data).toEqual({})
+  })
 
-//   it('should return 200 with "not allowed" message if user is not an admin', async () => {
-//     ;(getCurrentUser as jest.Mock).mockResolvedValue({
-//       isDomainAdmin: false,
-//       isGlobalAdmin: false,
-//       isAdmin: false,
-//       user: { email: 'user@example.com' },
-//     })
+  it('should return all data for GlobalAdmin', async () => {
+    ;(getCurrentUser as jest.Mock).mockResolvedValue({
+      isDomainAdmin: false,
+      isGlobalAdmin: true,
+      isAdmin: true,
+      user: { email: 'globaladmin@example.com' },
+    })
+    ;(Payment.find as jest.Mock).mockResolvedValue([
+      {
+        invoiceCreationDate: new Date('2024-01-10'),
+        generalSum: 100000,
+        type: 'debit',
+      },
+      {
+        invoiceCreationDate: new Date('2024-04-15'),
+        generalSum: 50000,
+        type: 'credit',
+      },
+    ])
 
-//     const { req, res } = createMocks({
-//       method: 'GET',
-//       query: {},
-//     })
+    const req = { method: 'GET', query: {} } as NextApiRequest
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as NextApiResponse
 
-//     await handler(req, res)
+    await handler(req, res)
 
-//     expect(res._getStatusCode()).toBe(200)
-//     const response = JSON.parse(res._getData())
-//     expect(response.message).toEqual('not allowed')
-//     expect(response.data).toEqual({})
-//   })
+    expect(res.status).toHaveBeenCalledWith(200)
+    const response = (res.json as jest.Mock).mock.calls[0][0]
 
-//   it('should return all data for GlobalAdmin', async () => {
-//     ;(getCurrentUser as jest.Mock).mockResolvedValue({
-//       isDomainAdmin: false,
-//       isGlobalAdmin: true,
-//       isAdmin: true,
-//       user: { email: 'globaladmin@example.com' },
-//     })
-//     ;(Payment.find as jest.Mock).mockResolvedValue([
-//       {
-//         invoiceCreationDate: new Date('2024-01-10'),
-//         generalSum: 100000,
-//         type: 'debit',
-//       },
-//       {
-//         invoiceCreationDate: new Date('2024-04-15'),
-//         generalSum: 50000,
-//         type: 'credit',
-//       },
-//     ])
+    expect(response.success).toBe(true)
+    expect(response.data.length).toBeGreaterThan(0)
+  })
 
-//     const { req, res } = createMocks({
-//       method: 'GET',
-//       query: {},
-//     })
+  it('should return 400 if an error occurs', async () => {
+    ;(getCurrentUser as jest.Mock).mockResolvedValue({
+      isDomainAdmin: true,
+      isGlobalAdmin: false,
+      isAdmin: false,
+      user: { email: 'admin@example.com' },
+    })
+    ;(Payment.find as jest.Mock).mockRejectedValue(new Error('Database error'))
 
-//     await handler(req, res)
+    const req = { method: 'GET', query: {} } as NextApiRequest
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as NextApiResponse
 
-//     expect(res._getStatusCode()).toBe(200)
-//     const response = JSON.parse(res._getData())
+    await handler(req, res)
 
-//     expect(response.success).toBe(true)
-//     expect(response.data.length).toBeGreaterThan(0)
-//   })
+    expect(res.status).toHaveBeenCalledWith(400)
+    const response = (res.json as jest.Mock).mock.calls[0][0]
+    expect(response.success).toBe(false)
+    expect(response.message).toEqual('Database error')
+  })
 
-//   it('should return 400 if an error occurs', async () => {
-//     ;(getCurrentUser as jest.Mock).mockResolvedValue({
-//       isDomainAdmin: true,
-//       isGlobalAdmin: false,
-//       isAdmin: false,
-//       user: { email: 'admin@example.com' },
-//     })
-//     ;(Payment.find as jest.Mock).mockRejectedValue(new Error('Database error'))
-
-//     const { req, res } = createMocks({
-//       method: 'GET',
-//       query: {},
-//     })
-
-//     await handler(req, res)
-
-//     expect(res._getStatusCode()).toBe(400)
-//     const response = JSON.parse(res._getData())
-//     expect(response.success).toBe(false)
-//     expect(response.message).toEqual('Database error')
-//   })
-// })
+})
