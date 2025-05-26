073127edf1a87769121559e9c57d54d403bf004c
zxclop <svyatoslav250706@gmail.com>
2025-05-26 17:22:24 +0300

    featureflags services

--- pages/api/feature-flags/index.ts
+++ pages/api/feature-flags/index.ts
@@ -1,38 +1,28 @@
-import FeatureFlags from '@modules/models/FeatureFlag'
+import FeatureFlagService from '@modules/services/FeatureFlagServices'
 import { getCurrentUser } from '@utils/getCurrentUser'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { isGlobalAdmin } = await getCurrentUser(req, res)
   if (!isGlobalAdmin) return res.status(403).json({ success: false })
-
+    
+    try {
   switch (req.method) {
-    case 'GET':
-      try {
-        const flags = await FeatureFlags.find()
-        return res.status(200).json({ success: true, data: flags })
-      } catch (error) {
-        return res.status(500).json({ success: false, message: error.message })
-      }
+    case 'GET': {
+      const flags = await FeatureFlagService.getAll()
+      return res.status(200).json({ success: true, data: flags })
+    } 
 
-    case 'POST':
-      try {
-        const { name, description, isEnabled } = req.body
-
-        const exists = await FeatureFlags.findOne({ name })
-        if (exists) {
-          return res.status(400).json({
-            success: false,
-            message: 'Feature flag with this name already exists',
-          })
-        }
-
-        const flag = await FeatureFlags.create({ name, description, isEnabled })
-        return res.status(201).json({ success: true, data: flag })
-      } catch (error) {
-        return res.status(500).json({ success: false, message: error.message })
-      }
+    case 'POST': {
+        const { name, description, isEnabled  } = req.body
+        const flag = await FeatureFlagService.create({ name, description, isEnabled  })
+        res.status(200).json({ success: true, data: flag })
+    }
 
     default:
       return res.status(405).end()
+  }
+    } catch (error: any) {
+        return res.status(500).json({ success: false, message: error.message })
   }
 }
