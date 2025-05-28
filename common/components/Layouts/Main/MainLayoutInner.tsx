0d9271de118473fdcc317caeef6bcd587ce6fd22
Vanya <51020174+VanyaRPP@users.noreply.github.com>
2025-05-28 19:31:12 +0300

    feat/add float buttons to layout (#959)
    * feat/add float buttons to layout
    
    * Feat: add fullscreen functionale to float buttons
    
    * Update store.ts
    
    * Update FloatButtonsLayoutAddon.tsx

--- common/components/Layouts/Main/MainLayoutInner.tsx
+++ common/components/Layouts/Main/MainLayoutInner.tsx
@@ -1,29 +1,40 @@
 'use client'
 
+import { FloatButtonsLayoutAddon } from './FloatButtonsLayoutAddon'
+import { BreadcrumbPath } from '@components/UI/Breadcrumb'
+import { Sidebar } from '@components/Layouts/Sidebar'
 import { Footer } from '@components/Layouts/Footer'
 import { Header } from '@components/Layouts/Header'
-import { Sidebar } from '@components/Layouts/Sidebar'
-import { BreadcrumbPath } from '@components/UI/Breadcrumb'
+import { FloatButtonItem } from '@utils/types'
 import { Layout } from 'antd'
+
 import styles from './style.module.scss'
 
 export interface MainLayoutProps {
   children: React.ReactNode
   path?: BreadcrumbPath[]
   simple?: boolean
+  floatButtons?: FloatButtonItem[]
 }
 
-const MainLayoutInner: React.FC<MainLayoutProps> = ({ children, path, simple }) => {
-  if (simple){
+const MainLayoutInner: React.FC<MainLayoutProps> = ({
+  children,
+  path,
+  simple,
+  floatButtons,
+}) => {
+  if (simple) {
     return (
       <Layout className={styles.SimpleWrapper}>
+        <FloatButtonsLayoutAddon buttons={floatButtons} />
         <Layout.Content className={styles.Content}>{children}</Layout.Content>
       </Layout>
     )
   }
-  
+
   return (
     <Layout hasSider>
+      <FloatButtonsLayoutAddon buttons={floatButtons} />
       <Sidebar collapsible className={styles.Sidebar} />
       <Layout>
         <Header className={styles.Header} path={path} />
