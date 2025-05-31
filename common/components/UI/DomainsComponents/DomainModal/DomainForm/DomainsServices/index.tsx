e1ff885fe4e233a1bbd3a02319c6f5ca891d6d4d
dmitrsavit <ex1kenny4543@gmail.com>
2025-05-31 16:55:23 +0300

    UpdatedLogic

--- common/components/UI/DomainsComponents/DomainModal/DomainForm/DomainsServices/index.tsx
+++ common/components/UI/DomainsComponents/DomainModal/DomainForm/DomainsServices/index.tsx
@@ -5,6 +5,7 @@ import {
 import { CloseOutlined, SaveOutlined } from '@ant-design/icons'
 import { Button, Card, Form, FormInstance, Input, Tooltip, message } from 'antd'
 import React, { FC, useEffect } from 'react'
+import { useWatch } from 'antd/es/form/Form'
 
 interface Props {
   form: FormInstance
@@ -21,99 +22,101 @@ const DomainsServices: FC<Props> = ({
   domainId,
   onCustomServicesChange,
 }) => {
-
   const [createCustomService] = useCreateCustomServiceMutation()
 
-  const handleSave = async (fieldKey: number) => {
-    const service = form.getFieldValue(['domainServices', fieldKey])
+  const allServices: { name?: string; _id?: string }[] =
+    Form.useWatch('domainServices', form) || []
+  const handleSave = async (fieldName: number) => {
+    const service = form.getFieldValue(['domainServices', fieldName])
+
     if (!service?.name) {
       message.error('Будь ласка, введіть назву послуги')
       return
     }
 
-    // const existingService = customServices?.data.find(
-    //   (s) => s.name === service?.name && s.domainId === domainId
-    // )
-    // if (existingService) {
-    //   message.info('Послуга з такою назвою вже існує')
-    //   return
-    // }
-
     try {
       const result = await createCustomService({
         name: service?.name,
       }).unwrap()
+
       const savedService = result.data
+      const updatedList = form
+        .getFieldValue('domainServices')
+        .map((s: any, idx: number) =>
+          idx === fieldName ? { ...s, _id: savedService._id } : s
+        )
+
       form.setFieldsValue({
-        domainServices: form
-          .getFieldValue('domainServices')
-          .map((s, idx) =>
-            idx === fieldKey ? { ...s, _id: savedService._id } : s
-          ),
+        domainServices: updatedList,
       })
+
       message.success('Послугу успішно збережено')
     } catch (error) {
       message.error('Помилка збереження послуги')
     }
   }
 
-  const handleRemove = (fieldName: number) => {
-    const updatedServices = form
-      .getFieldValue('domainServices')
-      .filter((_, idx) => idx !== fieldName)
-    form.setFieldsValue({ domainServices: updatedServices })
-  }
-
-  // if (isLoading) return <div>Завантаження...</div>
-  // if (error) {
-  //   return <div>Помилка завантаження даних: {JSON.stringify(error)}</div>
-  // }
-
   return (
     <Form.List name="domainServices">
       {(fields, { add, remove }) => (
-        <div style={{ display: 'flex', rowGap: 16, flexDirection: 'column' }}>
-          {fields.map((field) => (
-            <Card
-              size="small"
-              title={`Послуга ${field.name + 1}`}
-              key={field.key}
-              aria-disabled={!editable}
-              extra={
-                editable ? (
-                  <div>
-                    <Button type="link" onClick={() => handleSave(field.key)}>
-                      <SaveOutlined />
-                    </Button>
-                    <Button
-                      type="link"
-                      danger
-                      onClick={() => {
-                        remove(field.name)
-                        handleRemove(field.name)
-                      }}
-                    >
-                      <CloseOutlined />
-                    </Button>
-                  </div>
-                ) : null
-              }
-            >
-              <Form.Item label="Найменування" name={[field.name, 'name']}>
-                <Input
-                  placeholder="Найменування послуги"
-                  disabled={!editable}
-                />
-              </Form.Item>
-            </Card>
-          ))}
+        <div style={{ display: 'flex', flexDirection: 'column', rowGap: 16 }}>
+          {fields.map((field) => {
+            const watchedName = allServices[field.name]?.name || ''
+
+            return (
+              <Card
+                size="small"
+                title={`Послуга: ${watchedName || '(без назви)'}`}
+                key={field.key}
+                aria-disabled={!editable}
+                extra={
+                  editable && (
+                    <div>
+                      <Button
+                        type="link"
+                        onClick={() => {
+                          handleSave(field.name)
+                        }}
+                      >
+                        <SaveOutlined />
+                      </Button>
+                      <Button
+                        type="link"
+                        danger
+                        onClick={() => {
+                          remove(field.name)
+                        }}
+                      >
+                        <CloseOutlined />
+                      </Button>
+                    </div>
+                  )
+                }
+              >
+                <Form.Item
+                  {...field}
+                  label="Найменування"
+                  name={[field.name, 'name']}
+                  rules={[{ required: true, message: 'Вкажіть назву послуги' }]}
+                >
+                  <Input
+                    placeholder="Найменування послуги"
+                    disabled={!editable}
+                  />
+                </Form.Item>
+              </Card>
+            )
+          })}
+
           {editable && (
-            <Tooltip title="Якщо послуги зі списку вам не підходять, ви можете створити власну">
+            <Tooltip title="Якщо жодна стандартна послуга не підходить, додайте власну">
               <Button
                 type="dashed"
-                style={{ marginBottom: 10 }}
-                onClick={() => add()}
                 block
+                style={{ marginTop: 8 }}
+                onClick={() => {
+                  add()
+                }}
               >
                 + Додати послугу
               </Button>
