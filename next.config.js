e83aad0e866e4eade099b5a82f2987c0ad1cc6dd
Vanya <51020174+VanyaRPP@users.noreply.github.com>
2025-06-03 17:34:44 +0300

    feat: add PWA config (#974)
    * feat: add PWA config
    
    * upd
    
    * update manifest
    
    * upd icon
    
    * feat: add icons for pwa
    
    * feat: update pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -19,4 +24,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
