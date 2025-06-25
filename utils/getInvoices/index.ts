7bc9465576ebcc6e7df5d608770716fb89bd3847
MentoJl <vladganing32@gmail.com>
2025-06-25 14:23:50 +0300

    customInvoice Fix

--- utils/getInvoices/index.ts
+++ utils/getInvoices/index.ts
@@ -494,19 +494,18 @@ export const getCustomServiceInvoices = ({
   ) {
     return []
   }
-
-  const customServices = Array.isArray(service?.customServices)
-    ? service?.customServices.flatMap((customService) =>
-        Array.isArray(customService)
-          ? customService.map((s) => ({
-              name: s.label || 'Невідома послуга',
-              price: +toRoundFixed(s.price),
-              sum: +toRoundFixed(s.price),
-              type: ServiceType.Custom,
-              customService: true,
-            }))
-          : []
-      )
+  const customServices = Array.isArray(company?.customServices)
+    ? company?.customServices.flatMap((customService) => (
+        {
+          name: customService.label || 'Невідома послуга',
+          price: +toRoundFixed(customService.price),
+          sum: +toRoundFixed(customService.price),
+          type: ServiceType.Custom,
+          customService: true,
+        }
+      ))
     : []
+
   return customServices
 }
+
