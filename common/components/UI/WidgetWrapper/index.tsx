bd5973e90ab5b71850749ca376961d5d4073080e
dmitrsavit <141586035+ex1kenny@users.noreply.github.com>
2025-08-09 18:00:08 +0300

    Edit mode for dashboard page (#1054)
    * GridLayoutLibAndTables
    
    * panelka
    
    * Update drag & drop
    
    * addingSaveToLocalStorage
    
    * Update Drag & & Drop
    
    ---------
    
    Co-authored-by: MentoJl <vladganing32@gmail.com>
    Co-authored-by: dimasss777sss <d34362698@gmail.com>

---
+++ common/components/UI/WidgetWrapper/index.tsx
@@ -0,0 +1,40 @@
+
+import React, { FC, ReactNode } from 'react'
+import { useDynamicHeight } from '@modules/hooks/useDynamicHeight '
+import s from './style.module.scss'
+
+interface WidgetWrapperProps {
+  id: string
+  rowHeight: number
+  marginY: number
+  padding?: number
+  onHeightChange: (id: string, h: number) => void
+  children: ReactNode 
+  isEditMode 
+}
+
+export const WidgetWrapper: FC<WidgetWrapperProps> = ({
+  id,
+  rowHeight,
+  onHeightChange,
+  children,
+  marginY,
+  padding,
+  isEditMode,
+}) => {
+  const { ref } = useDynamicHeight({
+    nodeHeightCallback: (h) => onHeightChange(id, h),
+    gridRowHeight: rowHeight,
+    marginY,
+    padding,
+  })
+
+  return (
+    <div ref={ref} className={s.block}>
+      <div className={s.inner}>
+        {children}
+        {isEditMode && <div className={s.mask} />}
+      </div>
+    </div>
+  )
+}
