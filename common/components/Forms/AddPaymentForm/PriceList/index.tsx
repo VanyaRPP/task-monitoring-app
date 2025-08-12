25a3e560540bdfac4c2da96d6eae28b1efcbbe7e
Vanya <51020174+VanyaRPP@users.noreply.github.com>
2025-08-13 01:32:02 +0300

    Dev 13.08.25 (#1087)
    * Custum Field Value (#1081)
    
    * Fix Print View (#1083)
    
    Co-authored-by: korlak <58148263+korlak@users.noreply.github.com>
    
    * Revert 1082 revert 1078 print view (#1085)
    
    * Fix Print View (#1078)
    
    * Revert "Fix Print View (#1078)" (#1082)
    
    This reverts commit 660a7869e310468dfaf469b767c84df64e216dbb.
    
    * Revert "Revert "Fix Print View (#1078)" (#1082)"
    
    This reverts commit 7ccd1e295a5198f838211eb5c1aac7324127fbfc.
    
    ---------
    
    Co-authored-by: Vanya <51020174+VanyaRPP@users.noreply.github.com>
    Co-authored-by: korlak <58148263+korlak@users.noreply.github.com>
    
    * hot fix electricity (#1086)
    
    ---------
    
    Co-authored-by: dimasss777sss <d34362698@gmail.com>
    Co-authored-by: korlak <58148263+korlak@users.noreply.github.com>
    Co-authored-by: MentoJl <80547023+MentoJl@users.noreply.github.com>
    Co-authored-by: djproblematic <77460695+djproblematic@users.noreply.github.com>

--- common/components/Forms/AddPaymentForm/PriceList/index.tsx
+++ common/components/Forms/AddPaymentForm/PriceList/index.tsx
@@ -1,137 +1,35 @@
-import {
-  IPayment,
-  IPaymentField,
-} from '@common/api/paymentApi/payment.api.types'
-import { ServiceName } from '@utils/constants'
-import { Button, Table } from 'antd'
-import { ColumnsType } from 'antd/es/table'
 import { FC, useEffect, useRef, useState } from 'react'
-import styles from './styles.module.scss'
 import { useReactToPrint } from 'react-to-print'
 import { PrinterOutlined } from '@ant-design/icons'
 import dayjs from 'dayjs'
+import styles from './styles.module.scss'
 import GroupedPricesTable from '@components/Forms/GroupedReceiptForm/GroupedPricesTable'
-import { useGetCustomServicesByDomainQuery } from '@common/api/customServicesApi/customServices.api'
-interface InvoicesTableData extends IPaymentField {
-  number: number
-  unit: string
-}
-
-const columns: ColumnsType<InvoicesTableData> = [
-  {
-    title: '№',
-    dataIndex: 'number',
-    key: 'number',
-  },
-  {
-    title: 'Найменування робіт, послуг',
-    dataIndex: 'type',
-    key: 'type',
-    // render: (value, record, index) => { // old
-    //   return ServiceName[value]
-    // },
-  },
-  // { // olc
-  //   title: 'Кіль-сть',
-  //   dataIndex: 'amount',
-  //   key: 'amount',
-  // },
-  // { // old
-  //   title: 'Од.',
-  //   dataIndex: 'unit',
-  //   key: 'unit',
-  // },
-  // { // old
-  //   title: 'Ціна з ПДВ',
-  //   dataIndex: 'price',
-  //   key: 'price',
-  // },
-  {
-    title: 'Сума',
-    dataIndex: 'sum',
-    key: 'sum',
-  },
-]
+import { IPayment } from '@common/api/paymentApi/payment.api.types'
 
 const PriceList: FC<{ data: IPayment }> = ({ data }) => {
   const [payment, setPayment] = useState(data)
   const [totalSum, setTotalSum] = useState(0)
   const [totalFractionSum, setTotalFractionSum] = useState(0)
-  const { data: customDomainServices } = useGetCustomServicesByDomainQuery(
-    { domainId: [payment.domain._id] },
-    { skip: !payment.domain._id }
-  )
-
-  const getModifiedInvoices = () => {
-    // old
-    return payment.invoice.map(
-      (item, index) =>
-        ({
-          ...item,
-          unit: 'грн',
-          number: index + 1,
-        } as InvoicesTableData)
-    )
-  }
-
-  const groupedInvoices = (invoices: any, groups: any) => { // TODO: FIX maintenancePrice && rentPrice logic
-    const result =
-      groups?.map((group, index) => {
-        const groupInvoices = invoices?.filter((invoice) =>
-          group?.services?.some((service) =>
-            (invoice?.name === service?.name ||
-            invoice?.type === service?.fieldName) ||
-            (invoice?.type === 'maintenancePrice' && service?.fieldName === 'rentPrice')
-          )
-        )
-
-        const totalGroupSum = (groupInvoices ?? []).reduce((sum, invoice) => {
-          return sum + (invoice?.sum ?? 0)
-        }, 0)
-
-        return {
-          key: index,
-          number: index + 1,
-          type: group?.groupName,
-          unit: 'грн',
-          price: totalGroupSum.toFixed(2),
-          sum: totalGroupSum.toFixed(2),
-        }
-      }) || []
-
-    const customInvoice = invoices?.find(
-      (invoice) => invoice?.type === 'custom' && !invoice?.customService
-    )
-    if (customInvoice) {
-      result.push({
-        key: result.length,
-        number: result.length + 1,
-        type: customInvoice.name || 'custom',
-        unit: 'грн',
-        price: +customInvoice.sum,
-        sum: +customInvoice.sum,
-      })
-    }
-
-    return result
-  }
-  useEffect(() => {
-    setTotalSum(
-      payment.invoice.reduce((acc, item, index) => {
-        acc += Number(item.sum)
-        return acc
-      }, 0)
-    )
-  }, [payment])
-
-  useEffect(() => {
-    const splitSum = totalSum.toFixed(2).split('.')
-    setTotalFractionSum(Number(splitSum[1]))
-  }, [totalSum])
 
   useEffect(() => {
     setPayment(data)
   }, [data])
+
+  useEffect(() => {
+    const sum = payment.invoice.reduce((acc, item) => acc + Number(item.sum), 0)
+    setTotalSum(sum)
+
+    const [, fraction] = sum.toFixed(2).split('.')
+    setTotalFractionSum(Number(fraction))
+  }, [payment])
+
+  useEffect(() => {
+    const sum = payment.invoice.reduce((acc, item) => acc + Number(item.sum), 0)
+    setTotalSum(sum)
+
+    const [, fraction] = sum.toFixed(2).split('.')
+    setTotalFractionSum(Number(fraction))
+  }, [payment])
 
   const componentRef = useRef()
   const handlePrint = useReactToPrint({
@@ -144,7 +42,15 @@ const PriceList: FC<{ data: IPayment }> = ({ data }) => {
     <>
       <PrinterOutlined className={styles.print} onClick={handlePrint} />
       <div ref={componentRef}>
-        <div className={styles.container}>
+        <div
+          className={styles.container}
+          style={{
+            width: '100%',
+            height: '100%',
+            marginRight: '0.5em',
+            marginLeft: '0.5em',
+          }}
+        >
           <div className={styles.header}>
             <div className={styles.approvalSectionWrapper}>
               <div className={styles.approvalSection}>
@@ -158,128 +64,92 @@ const PriceList: FC<{ data: IPayment }> = ({ data }) => {
                 <div>
                   <strong>ЗАТВЕРДЖУЮ</strong>
                   <br />
-                  <div>
-                    <pre>
-                      {payment?.reciever?.description?.trim()} <br />
-                      {payment?.reciever?.companyName} <br />
-                      {payment?.reciever?.adminEmails?.map((email) => (
-                        <div key={email}>
-                          {email} <br />
-                        </div>
-                      ))}
-                    </pre>
-                  </div>
+                  <pre>
+                    {payment?.reciever?.description?.trim()} <br />
+                    {payment?.reciever?.companyName} <br />
+                    {payment?.reciever?.adminEmails?.map((email) => (
+                      <div key={email}>
+                        {email} <br />
+                      </div>
+                    ))}
+                  </pre>
                 </div>
               </div>
-              <div className={`${styles.approvalSection}`}>
-                <br />
-                <br />
-                <hr />
-              </div>
-              <div className={styles.approvalSection}>
-                <br />
-                <br />
-                <hr />
-              </div>
+              <div className={styles.approvalSection}><br /><br /><hr /></div>
+              <div className={styles.approvalSection}><br /><br /><hr /></div>
             </div>
           </div>
+
           <div className={styles.titleSection}>
-            <h1>
-              <b>АКТ надання послуг</b>
-            </h1>
+            <h1><b>АКТ надання послуг</b></h1>
             <p>
               <b>
                 № {payment?.invoiceNumber} від{' '}
-                {dayjs(payment?.invoiceCreationDate)?.format?.('DD.MM.YYYY')}{' '}
-                року.
+                {dayjs(payment?.invoiceCreationDate)?.format?.('DD.MM.YYYY')} року.
               </b>
             </p>
             <br />
             <hr />
           </div>
+
           <div className={styles.contentSection}>
             <p>
               Ми, що нижче підписалися, представник Замовника{' '}
-              {payment.reciever.description
-                ?.trim()
-                .replace(/(:\s)/g, ':\u00A0')}
-              , з одного боку, і представник Виконавця{' '}
-              {payment.provider.description
-                ?.trim()
-                .replace(/(:\s)/g, ':\u00A0')}
-              , з іншого боку, склали цей акт про те, що на підставі договору,
+              {payment.reciever.description?.trim().replace(/(:\s)/g, ':\u00A0')},
+              з одного боку, і представник Виконавця{' '}
+              {payment.provider.description?.trim().replace(/(:\s)/g, ':\u00A0')},
+              з іншого боку, склали цей акт про те, що на підставі договору,
               Виконавцем були виконані наступні роботи (надані такі послуги):
             </p>
-            <br/>
           </div>
-        </div>
 
-        <Table
-          columns={columns}
-          dataSource={groupedInvoices(
-            payment.invoice,
-            customDomainServices?.data
-          )}
-          pagination={false}
-          summary={() => {
-            return (
-              <Table.Summary.Row>
-                <Table.Summary.Cell index={0} colSpan={2}>
-                  Всього:
-                </Table.Summary.Cell>
-                <Table.Summary.Cell index={1}>
-                  {totalSum.toFixed(2)}
-                </Table.Summary.Cell>
-              </Table.Summary.Row>
-            )
-          }}
-        />
-        {/* <GroupedPricesTable preview domainId=''/> */}
-
-        <div className={styles.container}>
-          <div className={styles.contentSection}>
-            <div>
-              Загальна вартість робіт (послуг) склала без ПДВ{' '}
-              {totalSum.toFixed(0)} гривень {totalFractionSum} копійок, ПДВ 0
-              гривень 00 копійок, загальна вартість робіт (послуг) із ПДВ{' '}
-              {totalSum.toFixed(0)} гривень {totalFractionSum} копійок.
-              <br />
-              Замовник претензій по об&apos;єму, якості та строкам виконання
-              робіт (надання послуг) не має.
-            </div>
-            <br/>
+          <div style={{ marginTop: '1em' }}>
+            <GroupedPricesTable
+              preview
+              domainId={payment?.domain?._id}
+              invoices={payment.invoice}
+            />
           </div>
-          <hr />
-          <br />
-          <div className={styles.signaturesSection}>
-            <div className={styles.signatureBlock}>
+
+          <div className={styles.contaiіner}>
+            <div className={styles.contentSection}>
               <div>
-                <b>Від Виконавця</b>
+                Загальна вартість робіт (послуг) склала без ПДВ{' '}
+                {totalSum.toFixed(0)} гривень {totalFractionSum} копійок, ПДВ 0
+                гривень 00 копійок, загальна вартість робіт (послуг) із ПДВ{' '}
+                {totalSum.toFixed(0)} гривень {totalFractionSum} копійок.
                 <br />
-                <br />
-                <hr />
-                <br />
-                <b>
-                  {new Date(payment.invoiceCreationDate).toLocaleDateString()}
-                </b>{' '}
-                <br />
-                <pre>
-                  {payment.provider.description?.trim()} <br />
-                </pre>
+                Замовник претензій по об’єму, якості та строкам виконання
+                робіт (надання послуг) не має.
               </div>
+              <br />
             </div>
-            <div className={styles.signatureBlock}>
-              <div>
-                <b>Від Замовника</b>
-                <br />
-                <br />
-                <hr />
-                <br />
-                <b>
-                  {new Date(payment.invoiceCreationDate).toLocaleDateString()}
-                </b>{' '}
-                <br />
-                <pre>{payment.reciever.description?.trim()}</pre>
+            <hr />
+            <br />
+            <div className={styles.signaturesSection}>
+              <div className={styles.signatureBlock}>
+                <div>
+                  <b>Від Виконавця</b>
+                  <br />
+                  <br />
+                  <hr />
+                  <br />
+                  <b>{new Date(payment.invoiceCreationDate).toLocaleDateString()}</b>
+                  <br />
+                  <pre>{payment.provider.description?.trim()}</pre>
+                </div>
+              </div>
+              <div className={styles.signatureBlock}>
+                <div>
+                  <b>Від Замовника</b>
+                  <br />
+                  <br />
+                  <hr />
+                  <br />
+                  <b>{new Date(payment.invoiceCreationDate).toLocaleDateString()}</b>
+                  <br />
+                  <pre>{payment.reciever.description?.trim()}</pre>
+                </div>
               </div>
             </div>
           </div>
