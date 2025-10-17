a2892f24472f85cebffdecb2d49acfefe41f55f1
MentoJl <80547023+MentoJl@users.noreply.github.com>
2025-10-17 14:46:32 +0300

    fix price kW

--- common/components/Losses/LossesCollapse.tsx
+++ common/components/Losses/LossesCollapse.tsx
@@ -24,125 +24,98 @@ export const LossesCollapse = ({
   const [consumed, setConsumed] = useState(null)
   const [total, setTotal] = useState(null)
   const [losses, setLosses] = useState(null)
+  const [pricekWH, setPricekWH] = useState(0)
+  const customServices = Form.useWatch('customServices', form)
+
+  useEffect(() => {
+    const electricityService = customServices?.find(
+      item => item?.fieldName === 'electricityPrice'
+    );
+    setPricekWH(electricityService?.price ?? 0);
+  }, form.getFieldValue('customServices'))
 
   useEffect(() => {
     if (consumed && total) {
-      const rawPrice = Number(
-        form.getFieldValue(['customServices', 5, 'price'])
-      )
+      const rawPrice = pricekWH;
+      const tariff = isVAT ? Number(rawPrice) : Number(rawPrice) / 1.2;
 
-      const tariff = isVAT
-        ? Number(rawPrice)
-        : Number((Number(rawPrice) / 1.2).toFixed(8))
+      const priceWithTariff = tariff * Number(consumed);
 
-      const priceWithTariff = tariff * Number(consumed)
+      const lossesPercent = ((Number(total) - priceWithTariff) / priceWithTariff) * 100;
 
-      const lossesPercent =
-        ((Number(total) - priceWithTariff) / priceWithTariff) * 100
-
-      const formatted = lossesPercent.toFixed(6)
-      setLosses(formatted)
-      form.setFieldsValue({ losses: formatted })
+      const formatted = lossesPercent.toFixed(2);
+      setLosses(formatted);
+      form.setFieldsValue({ losses: formatted });
     }
-  }, [consumed, total, isVAT])
+  }, [consumed, total, isVAT]);
 
   return (
-    <Collapse size="small" bordered={false}>
-      <Collapse.Panel
-        style={{
-          marginBottom: -24,
-        }}
-        header={
-          <Form.Item
-            label="Втрати в трансформаторі, лініях, реактивна (%)"
-            name={name}
-          >
-            <InputNumber
-              parser={inputNumberParser}
-              placeholder="Втрати електроенергії"
-              disabled={disabled}
-              style={{
-                width: disabled ? '100%' : !isServiceForm ? 365 : 395,
-              }}
-            />
-          </Form.Item>
-        }
-        key="1"
-      >
-        <Space
-          direction="horizontal"
+  <Collapse
+    size='small'
+    bordered={false}
+  >
+    <Collapse.Panel 
+      style={{
+        marginBottom: -24,
+      }}
+      header={
+        <Form.Item 
+          label="Втрати в трансформаторі, лініях, реактивна (%)"
+          name={name}
+        >
+          <InputNumber
+            parser={inputNumberParser}
+            placeholder="Втрати електроенергії"
+            disabled={disabled}
+            style={{
+              width: disabled ? '100%' : !isServiceForm ? 365 : 395,
+            }}
+          />
+        </Form.Item>
+      }
+      key="1"
+    >
+    <Space direction='horizontal' style={{ marginBottom: 16, width: '100%', justifyContent: 'space-evenly' }}>
+      <Space direction='vertical'>
+        <Text>Спожито (кВт)</Text>
+        <InputNumber placeholder='Спожито' style={{ width: 150 }} onChange={(value) => setConsumed(value)}></InputNumber>
+        <div/>
+        <Text>Ціна кВт: {isVAT ? pricekWH : (pricekWH / 1.2).toFixed(2)}</Text>
+      </Space>
+      <Space direction='vertical'>
+        <Text>Всього (грн без ПДВ)</Text>
+        <InputNumber placeholder='Загальне' style={{ width: 150 }} onChange={(value) => setTotal(value)}></InputNumber>
+        <div/>
+        <Checkbox
+          checked={isVAT}
+          onChange={(e) => setIsVAT(e.target.checked)}
+        >
+          ПДВ (+20%)
+        </Checkbox>
+      </Space>
+    </Space>
+    <br/>
+    <Row
+      align="middle"
+      justify="center"
+      style={{ fontSize: 20, fontFamily: 'serif', lineHeight: 1.2 }}
+    >
+      <Col style={{ textAlign: 'center', marginRight: 8 }}>
+        {total || 'Загальне'} - ({isVAT ? pricekWH : (pricekWH / 1.2).toFixed(2)} * {consumed || 'Спожито'})
+        <div
           style={{
             marginBottom: 16,
             width: '100%',
             justifyContent: 'space-evenly',
           }}
-        >
-          <Space direction="vertical">
-            <Text>Спожито (кВт)</Text>
-            <InputNumber
-              placeholder="Спожито"
-              style={{ width: 150 }}
-              onChange={(value) => setConsumed(value)}
-            ></InputNumber>
-            <div />
-            <Text>
-              Ціна кВт:{' '}
-              {isVAT
-                ? form.getFieldValue(['customServices', 5, 'price'])
-                : (
-                    form.getFieldValue(['customServices', 5, 'price']) / 1.2
-                  ).toFixed(2)}
-            </Text>
-          </Space>
-          <Space direction="vertical">
-            <Text>Всього (грн без ПДВ)</Text>
-            <InputNumber
-              placeholder="Загальне"
-              style={{ width: 150 }}
-              onChange={(value) => setTotal(value)}
-            ></InputNumber>
-            <div />
-            <Checkbox
-              checked={isVAT}
-              onChange={(e) => setIsVAT(e.target.checked)}
-            >
-              ПДВ (+20%)
-            </Checkbox>
-          </Space>
-        </Space>
-        <br />
-        <Row
-          align="middle"
-          justify="center"
-          style={{ fontSize: 20, fontFamily: 'serif', lineHeight: 1.2 }}
-        >
-          <Col style={{ textAlign: 'center', marginRight: 8 }}>
-            {total || 'Загальне'} - (
-            {isVAT
-              ? form.getFieldValue(['customServices', 5, 'price'])
-              : (
-                  form.getFieldValue(['customServices', 5, 'price']) / 1.2
-                ).toFixed(8)}{' '}
-            * {consumed || 'Спожито'})
-            <div
-              style={{
-                borderTop: '1px solid',
-                margin: '2px 0',
-              }}
-            />
-            (
-            {isVAT
-              ? form.getFieldValue(['customServices', 5, 'price'])
-              : (
-                  form.getFieldValue(['customServices', 5, 'price']) / 1.2
-                ).toFixed(8)}{' '}
-            * {consumed || 'Спожито'})
-          </Col>
-          <Col>
-            <Text>× 100 ≈ {losses}%</Text>
-          </Col>
-        </Row>
-      </Collapse.Panel>
-    </Collapse>
-  )
+        />
+        ({isVAT ? pricekWH : (pricekWH / 1.2).toFixed(2)} * {consumed || 'Спожито'})
+      </Col>
+      <Col>
+        <Text>× 100 ≈ {losses}%</Text>
+      </Col>
+    </Row>
+
+    </Collapse.Panel>
+  </Collapse>)
 }
