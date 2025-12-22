39a00a42f5aa2ceeb010a0d15517d9e5b29eea73
dimasss777sss <dvoytenko.dev@gmail.com>
2025-12-22 21:12:03 +0200

    Title page E-orenda (#1216)

--- pages/dashboard/tables.tsx
+++ pages/dashboard/tables.tsx
@@ -1,14 +1,9 @@
 import MainLayout from '@common/components/Layouts/Main'
 import DashboardPage from '@common/components/DashboardPage'
 import { AppRoutes } from '@utils/constants'
-import Head from 'next/head'
 
 export default function TablesPage() {
   return (
-    <>
-    <Head>
-      <title>Всі таблиці</title>
-    </Head>
     <MainLayout
       path={[
         { title: 'Панель управління', path: AppRoutes.INDEX },
@@ -17,6 +12,5 @@ export default function TablesPage() {
     >
       <DashboardPage />
     </MainLayout>
-    </>
   )
 }
