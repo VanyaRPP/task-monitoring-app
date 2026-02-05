cbd75ad29adde88e0314dfdfacdd365449d6f17c
subyea <maxkingost@gmail.com>
2026-02-05 18:04:12 +0200

    СustomServicesCard test

---
+++ common/components/UI/CustomServicesCard/CustomServicesCard.test.tsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { Form } from 'antd'
+import CustomServicesCard from './index'
+import { Roles } from '@utils/constants'
+
+jest.mock('@common/api/userApi/user.api', () => ({
+  useGetCurrentUserQuery: jest.fn(),
+}))
+
+jest.mock('@utils/helpers', () => ({
+  inputNumberParser: jest.fn((v) => v),
+}))
+
+const mockUseGetCurrentUserQuery = require('@common/api/userApi/user.api').useGetCurrentUserQuery as jest.Mock
+const useWatchSpy = jest.spyOn(Form, 'useWatch')
+
+const ALL_CUSTOM_SERVICES = [
+  { _id: '1', label: 'Послуга 1', fieldName: 'service1' },
+]
+
+const renderWithForm = (ui: React.ReactElement) => {
+  const Wrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [form] = Form.useForm()
+    return <Form form={form}>{children}</Form>
+  }
+  return render(ui, { wrapper: Wrapper })
+}
+
+describe('CustomServicesCard', () => {
+  const setFieldsValueMock = jest.fn()
+  const defaultMockUser = { data: { roles: [Roles.DOMAIN_ADMIN] } }
+
+  beforeEach(() => {
+    mockUseGetCurrentUserQuery.mockReturnValue(defaultMockUser)
+    useWatchSpy.mockReturnValue([])
+    setFieldsValueMock.mockClear()
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('renders add button when not disabled and not service form', () => {
+    renderWithForm(
+      <CustomServicesCard
+        form={{ setFieldsValue: setFieldsValueMock }}
+        allCustomServices={ALL_CUSTOM_SERVICES}
+        disabled={false}
+        isServiceForm={false}
+      />
+    )
+
+    expect(screen.getByText('Індивідуальні послуги')).toBeInTheDocument()
+  })
+
+  test('adds custom service from dropdown', () => {
+    renderWithForm(
+      <CustomServicesCard
+        form={{ setFieldsValue: setFieldsValueMock }}
+        allCustomServices={ALL_CUSTOM_SERVICES}
+        disabled={false}
+        isServiceForm={false}
+      />
+    )
+
+    fireEvent.click(screen.getByText('Індивідуальні послуги'))
+    fireEvent.click(screen.getByText('Послуга 1'))
+
+    expect(setFieldsValueMock).toHaveBeenCalledWith({
+      customServices: [
+        {
+          _id: '1',
+          label: 'Послуга 1',
+          fieldName: 'service1',
+          price: 0,
+        },
+      ],
+    })
+  })
+})
