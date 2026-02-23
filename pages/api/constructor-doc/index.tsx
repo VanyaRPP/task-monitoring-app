81d81c11ab356424b4f189ae89328b640373d9eb
subyea <maxkingost@gmail.com>
2026-02-23 19:15:31 +0200

    added route

---
+++ pages/api/constructor-doc/index.tsx
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import start, { Data } from '@pages/api/api.config'
+
+start()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case 'POST':
+      try {
+        const { domainId, meta, authTag } = req.body
+
+        return res.status(201).json({ success: true, data: { domainId, meta, authTag } })
+      } catch (error) {
+        return res.status(400).json({ success: false, error: error })
+      }
+  }
+}
