1f06d45005cc147d06bdc4188b59e32d3eaa399c
subyea <maxkingost@gmail.com>
2026-03-24 17:25:29 +0200

    parts have been moved to separate files

---
+++ common/components/Tables/PaymentsBulk/cells/Cleaning/index.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react"
+import { useInvoicesPaymentContext } from "@components/DashboardPage/blocks/paymentsBulk"
+import { Form, InputNumber } from 'antd'
+import { ServiceType } from "@utils/constants"
+import { toRoundFixed, inputNumberParser } from "@utils/helpers"
+import validator from "@utils/validator" 
+
+export const Cleaning: React.FC<{ name: number }> = ({ name }) => {
+  const { form } = useInvoicesPaymentContext()
+
+  const cleaning: boolean =
+    Form.useWatch(['payments', name, 'company', 'cleaning'], form) ?? false
+
+  const price: number =
+    Form.useWatch(
+      ['payments', name, 'invoice', ServiceType.Cleaning, 'price'],
+      form
+    ) ?? 0
+
+  useEffect(() => {
+    if (cleaning) {
+      const sum = +toRoundFixed(price ?? 0)
+      form.setFieldValue(
+        ['payments', name, 'invoice', ServiceType.Cleaning, 'sum'],
+        sum
+      )
+    }
+  }, [form, name, price, cleaning])
+
+  if (cleaning) {
+    return (
+      <Form.Item
+        name={[name, 'invoice', ServiceType.Cleaning, 'price']}
+        style={{ margin: 0 }}
+        rules={[validator.required(), validator.min(0)]}
+      >
+        <InputNumber parser={inputNumberParser} style={{ width: 'auto' }} />
+      </Form.Item>
+    )
+  }
+}
---
+++ common/components/Tables/PaymentsBulk/cells/Maintenance/index.tsx
@@ -0,0 +1,68 @@
+import { useEffect } from 'react'
+import { useInvoicesPaymentContext } from '@common/components/DashboardPage/blocks/paymentsBulk'
+import { Form, Tooltip, InputNumber, Input } from 'antd'
+import validator from '@utils/validator'
+import { ServiceType } from '@utils/constants'
+import { inputNumberParser, toRoundFixed } from '@utils/helpers'
+import { QuestionCircleOutlined } from '@ant-design/icons'
+
+
+export const MaintenancePrice: React.FC<{ name: number }> = ({ name }) => {
+  const { form } = useInvoicesPaymentContext()
+
+  const servicePricePerMeter: number =
+    Form.useWatch(
+      ['payments', name, 'company', 'servicePricePerMeter'],
+      form
+    ) ?? 0
+
+  return (
+    <Tooltip
+      title={
+        !!servicePricePerMeter &&
+        'Індивідуальне утримання, що передбачене договором'
+      }
+    >
+      <Form.Item
+        name={[name, 'invoice', ServiceType.Maintenance, 'price']}
+        style={{ margin: 0 }}
+        rules={[validator.required(), validator.min(0)]}
+      >
+        <InputNumber
+          style={{ width: 'auto' }}
+          parser={inputNumberParser}
+          suffix={!!servicePricePerMeter && <QuestionCircleOutlined />}
+        />
+      </Form.Item>
+    </Tooltip>
+  )
+}
+
+export const MaintenanceSum: React.FC<{ name: number }> = ({ name }) => {
+  const { form } = useInvoicesPaymentContext()
+
+  const totalArea: number =
+    Form.useWatch(['payments', name, 'company', 'totalArea'], form) ?? 0
+  const price: number =
+    Form.useWatch(
+      ['payments', name, 'invoice', ServiceType.Maintenance, 'price'],
+      form
+    ) ?? 0
+
+  useEffect(() => {
+    form.setFieldValue(
+      ['payments', name, 'invoice', ServiceType.Maintenance, 'sum'],
+      +toRoundFixed(+totalArea * +price)
+    )
+  }, [form, name, totalArea, price])
+
+  return (
+    <Form.Item
+      name={[name, 'invoice', ServiceType.Maintenance, 'sum']}
+      style={{ margin: 0 }}
+      rules={[validator.required(), validator.min(0)]}
+    >
+      <Input type="number" disabled />
+    </Form.Item>
+  )
+}
