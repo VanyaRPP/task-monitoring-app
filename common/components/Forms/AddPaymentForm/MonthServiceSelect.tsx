35b2835235f02c95c048337b28fc3b214be7a486
zxclop <svyatoslav250706@gmail.com>
2026-04-09 14:48:10 +0300

    fix by del services

--- common/components/Forms/AddPaymentForm/MonthServiceSelect.tsx
+++ common/components/Forms/AddPaymentForm/MonthServiceSelect.tsx
@@ -1,4 +1,3 @@
-import { getFormattedDate } from '@assets/features/formatDate'
 import { validateField } from '@assets/features/validators'
 import { useGetAllServicesQuery } from '@common/api/serviceApi/service.api'
 import { Form, FormInstance, Select } from 'antd'
@@ -16,7 +15,7 @@ const MonthServiceSelect: React.FC<MonthServiceSelectProps> = ({
 }) => {
   const streetId: string = Form.useWatch('street', form)
   const domainId: string = Form.useWatch('domain', form)
-  const serviceId: string = Form.useWatch('service', form)
+  const monthService: string = Form.useWatch('monthService', form)
 
   const {
     data: { data: services } = { data: [] },
@@ -31,34 +30,28 @@ const MonthServiceSelect: React.FC<MonthServiceSelectProps> = ({
   )
 
   const options = useMemo(() => {
-  if (services?.length > 0) {
-    return services.map((i) => ({
-      value: i._id,
-      label: getFormattedDate(i.date, 'MMMM YYYY'),
-    }));
-  }
-
-  return Array.from({ length: 12 }, (_, i) => {
-    const month = dayjs().subtract(i, 'month').startOf('month');
-    return {
-      value: month.toISOString(),
-      label: month.format('MMMM YYYY'),
-    };
-  });
-}, [services]);
+    const serviceMonths = services?.map(i => dayjs(i.date).startOf('month').toISOString()) || []
+    const currentMonths = Array.from({ length: 12 }, (_, i) => dayjs().subtract(i, 'month').startOf('month').toISOString())
+    const allMonths = [...new Set([...serviceMonths, ...currentMonths])]
+    allMonths.sort((a, b) => dayjs(b).diff(dayjs(a)))
+    return allMonths.map(month => ({
+      value: month,
+      label: dayjs(month).format('MMMM YYYY'),
+    }))
+  }, [services])
 
   useEffect(() => {
     if (!edit) {
       if (options.length === 1) {
         form.setFieldsValue({ monthService: options[0].value })
       } else if (
-        !serviceId ||
-        !options.some((option) => option.value === serviceId)
+        !monthService ||
+        !options.some((option) => option.value === monthService)
       ) {
         form.setFieldsValue({ monthService: options[0]?.value })
       }
     }
-  }, [form, options, serviceId, edit])
+  }, [form, options, monthService, edit])
 
   return (
     <Form.Item
