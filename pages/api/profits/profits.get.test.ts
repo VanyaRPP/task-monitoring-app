330104b7d79a18a817109faf1396f6fce7513c31
zxclop <163200974+zxclop@users.noreply.github.com>
2026-05-01 11:57:11 +0300

    Revert "profit (#1495)" (#1499)
    This reverts commit 97c72416dfe3f7c03fb40c2d4aefb93adbcc6962.

--- pages/api/profits/profits.get.test.ts
+++ pages/api/profits/profits.get.test.ts
@@ -1,6 +1,4 @@
 import { expect } from '@jest/globals'
-if (typeof global.fetch === 'undefined') 
-  global.fetch = jest.fn() as any
 import { mockLoginAs } from '@utils/mockLoginAs'
 import { setupTestEnvironment } from '@utils/setupTestEnvironment'
 import { profits, users, domains } from '@utils/testData'
