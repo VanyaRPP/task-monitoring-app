63064459c49de89b18df6ac2249f70753e1abe84
mary <marinadenis2018@gmail.com>
2026-05-18 15:31:39 +0300

    Fix visual Animation
    https://app.asana.com/1/1202389091502512/project/1214169728867936/task/1214762600104275?focus=true

--- common/components/HomePage/index.tsx
+++ common/components/HomePage/index.tsx
@@ -2,36 +2,43 @@ import LottieAnimation from '@components/UI/LottieAnimation'
 import SplashCursor from '@components/UI/SplashCursor'
 import HomePageTitle from '@assets/svg/homePageTitle'
 import { AppRoutes } from '@utils/constants'
-import { isProd } from '@utils/env'
 import { LogoIcon } from '@assets/icon/Logo'
 import { Button, Typography } from 'antd'
-import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import CardPage from '@components/CardSwapper'
-import ScrollFactoryAnimation from '@components/ScrollFactoryAnimation'
 import s from './style.module.scss'
 
 const HomePage: React.FC = () => {
   const router = useRouter()
   return (
-    <>
-      {/* <SplashCursor /> */}
-      <div className={s.HomePage}>
-        {/* <div className={s.Logo}>
-          <LogoIcon style={{ fontSize: '32px' }} />
-          <HomePageTitle />
-        </div> */}
+    <div className={s.HomePage}>
+      <SplashCursor />
 
+      <div style={{ position: 'absolute', inset: 0, zIndex: 0, pointerEvents: 'none', overflow: 'hidden' }}>
+        <LottieAnimation
+          src="/animations/WaveForBG.json"
+          loop
+          style={{
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+            transform: 'rotate(180deg) scale(1.05)', 
+          }}
+        />
+      </div>
+
+      <div style={{ position: 'relative', zIndex: 10, display: 'flex', flexDirection: 'column', flex: 1 }}>
         <div className={s.Header}>
+          <div className={s.Logo}>
+            <LogoIcon style={{ fontSize: '100px', color: 'white' }} />
+            <HomePageTitle />
+          </div>
+
           <div className={s.Buttons}>
             <Button
               type="primary"
               onClick={() => {
-                if (isProd) {
-                  signIn('google')
-                } else {
-                  router.push(AppRoutes.AUTH_SIGN_IN)
-                }
+                router.push(AppRoutes.AUTH_SIGN_IN)
               }}
             >
               Увійти
@@ -40,6 +47,7 @@ const HomePage: React.FC = () => {
             <Button
               ghost
               type="primary"
+              className={s.Button}
               onClick={() => {
                 router.push(AppRoutes.CONTACTS)
               }}
@@ -49,9 +57,31 @@ const HomePage: React.FC = () => {
           </div>
         </div>
 
-        <ScrollFactoryAnimation />
+        <div className={s.Container}>
+          <div className={s.HalfBlock}>
+            <div className={s.TextGlassCard}>
+              <Typography.Title level={2} style={{ fontSize: '2rem', color: 'white' }}>
+                Ласкаво просимо до E-ORENDA!
+              </Typography.Title>
+              <Typography.Paragraph style={{ fontSize: '1.3rem', color: 'rgba(255,255,255,0.85)' }}>
+                Керуйте процесом надання послуг нерухомості та систематизуйте
+                відносини між користувачами за допомогою нашого сайту! Ресурс
+                допоможе з автоматичним розрахунком платежів та своєчасним
+                формуванням та виставленням рахунків. Вам, як надавачу послуг,
+                платформа дозволить легко впоратися з усіма аспектами
+                користування нерухомим майном. Забезпечте собі простоту,
+                ефективність та зручність в управлінні!
+              </Typography.Paragraph>
+            </div>
+          </div>
+          <div className={s.HalfBlock}>
+            <div className={s.cardPage}>
+              <CardPage />
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
